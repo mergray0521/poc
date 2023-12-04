d51931a4c690ebc5eb543164e4135f84ad8177bb
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 08:54:23 -0500

    Update passport_app.py

--- passport_app.py
+++ passport_app.py
@@ -1,6 +1,7 @@
 git clone https://github.com/snowflakedb/snowflake-connector-python.git
 
-cd snowflake-connector-python
+git add .
+git commit -m "Your commit message here"
 
-
+git push origin poc
 
