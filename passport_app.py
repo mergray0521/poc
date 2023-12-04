ef3eecaacbd92756c9e4ca581edf1326bfd14314
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 12:28:28 -0500

    Update passport_app.py
    trying form

--- passport_app.py
+++ passport_app.py
@@ -1,16 +1,40 @@
-import streamlit 
+import streamlit as st
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
+st.title('My Passport')
+st.header('Create Token Schema')
 
-my_cur.execute("select * from token_schemas")
-my_data_rows = my_cur.fetchall()
-streamlit.text("Current token schemas include:")
-streamlit.text(my_data_rows)
+def main():
+    st.title("Token Information Form")
 
+    # Create input fields for token information
+    token_name = st.text_input("Token Name", "")
+    fungibility = st.selectbox("Fungibility", ["Fungible", "Non-Fungible"])
+    ip = st.text_input("IP Address", "")
+    token_admin = st.text_input("Token Admin", "")
+    metadata = st.text_area("Metadata", "")
 
+    # Submit button
+    if st.button("Submit"):
+        # Process the form data (you can replace this with your logic)
+        result = {
+            "Token Name": token_name,
+            "Fungibility": fungibility,
+            "IP Address": ip,
+            "Token Admin": token_admin,
+            "Metadata": metadata
+        }
+
+        # Display the result
+        st.success("Form submitted successfully!")
+        st.write("Result:")
+        st.write(result)
+
+if __name__ == "__main__":
+    main()
 
 
   
