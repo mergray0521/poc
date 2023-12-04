0ba001764e47d9a8f833679e8b8474fd0be826b5
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-03 21:55:15 -0500

    Update passport_app.py
    just python + login

--- passport_app.py
+++ passport_app.py
@@ -14,17 +14,3 @@ warehouse = "compute_wh"
 database = "token_schemas"
 schema = "token_schemas"
 client_session_keep_alive = true
-
-# streamlit_app.py
-
-import streamlit as st
-
-# Initialize connection.
-conn = st.connection("snowflake")
-
-# Perform query.
-df = conn.query("SELECT * from token_schemas;", ttl=600)
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.NAME} has a :{row.token_schema_id}:")
