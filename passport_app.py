c8ef5d4e0e5e156efeb52b54ff597c69ecb90b17
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 11:51:48 -0500

    Update passport_app.py
    my_cnx added

--- passport_app.py
+++ passport_app.py
@@ -3,35 +3,12 @@ import pandas
 import requests
 import snowflake.connector
 
+    # Connect to Snowflake
+    my_cnx = connect_to_snowflake()
+
+    
+
+
 streamlit.title('My Passport')
 streamlit.header('Create Token Schema')
 
-
-def main():
-    st.title("Token Schema Form")
-
-    # Create input fields for token information
-    token_name = st.text_input("Token Name", "")
-    fungibility = st.selectbox("Fungibility", ["Fungible", "Non-Fungible"])
-    ip = st.text_input("IP Address", "")
-    token_admin = st.text_input("Token Admin", "")
-    metadata = st.text_area("Metadata", "")
-
-    # Submit button
-    if st.button("Submit"):
-        # Process the form data (you can replace this with your logic)
-        result = {
-            "Token Name": token_name,
-            "Fungibility": fungibility,
-            "IP Address": ip,
-            "Token Admin": token_admin,
-            "Metadata": metadata
-        }
-
-        # Display the result
-        st.success("Form submitted successfully!")
-        st.write("Result:")
-        st.write(result)
-
-if __name__ == "__main__":
-    main()
