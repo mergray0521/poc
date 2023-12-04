11397903f70a43a0d76ce31e312ee89efca72471
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-03 21:13:44 -0500

    Update passport_app.py
    connect sf account

--- passport_app.py
+++ passport_app.py
@@ -4,3 +4,13 @@ streamlit.title('Welcome to Passport')
 streamlit.header('Create Token Schema')
 
 pip install "snowflake-connector-python"
+
+[connections.snowflake]
+account = "CNAHFEW.KNB62637"
+user = "Mergray0521"
+password = "Lakegage0521!"
+role = "accountadmin"
+warehouse = "compute_wh"
+database = "token_schemas"
+schema = "token_schemas"
+client_session_keep_alive = true
