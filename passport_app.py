8e717712abf45c2092dcbb737835cb8b1a7761bf
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-03 22:08:59 -0500

    Update passport_app.py
    new import snowflake + login

--- passport_app.py
+++ passport_app.py
@@ -3,14 +3,32 @@ import streamlit
 streamlit.title('Welcome to Passport')
 streamlit.header('Create Token Schema')
 
-pip install "snowflake-connector-python"
+import snowflake.connector
 
-[connections.snowflake]
-account = "CNAHFEW.KNB62637"
-user = "Mergray0521"
-password = "Lakegage0521!"
-role = "accountadmin"
-warehouse = "compute_wh"
-database = "token_schemas"
-schema = "token_schemas"
-client_session_keep_alive = true
+# Set up the connection parameters
+connection_params = {
+    'user': 'mergray0521',
+    'password': 'Lakegage0521!',
+    'account': 'CNAHFEW.KNB62637',
+    'warehouse': 'compute_wh',
+    'database': 'token_schemas',
+    'schema': 'token_schemas'
+}
+
+# Establish the connection
+conn = snowflake.connector.connect(**connection_params)
+
+# Create a cursor object
+cursor = conn.cursor()
+
+# Execute SQL queries or commands here
+cursor.execute("SELECT * FROM token_schemas")
+
+# Fetch the results
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
