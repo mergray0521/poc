8e17b9b21d01b76990ff6e2cdbb97a01138c7885
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 12:31:52 -0500

    Update passport_app.py
    added more options

--- passport_app.py
+++ passport_app.py
@@ -4,17 +4,16 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-st.title('My Passport')
-st.header('Create Token Schema')
+st.title('Create Token Schema')
 
 def main():
-    st.title("Token Information Form")
+    st.header("Token Information Form")
 
     # Create input fields for token information
     token_name = st.text_input("Token Name", "")
-    fungibility = st.selectbox("Fungibility", ["Fungible", "Non-Fungible"])
-    ip = st.text_input("IP Address", "")
-    token_admin = st.text_input("Token Admin", "")
+    fungibility = st.selectbox("Fungibility", ["Fungible", "Non-Fungible", "Semi-Fungible"])
+    ip = st.text_input("IP", ["HTTYD", "HHN", "CINEPHILE", "TOOTHSOME"])
+    token_admin = st.text_input("Token Admin", ["Miach", "Steve", Mere"])
     metadata = st.text_area("Metadata", "")
 
     # Submit button
@@ -23,7 +22,7 @@ def main():
         result = {
             "Token Name": token_name,
             "Fungibility": fungibility,
-            "IP Address": ip,
+            "IP": ip,
             "Token Admin": token_admin,
             "Metadata": metadata
         }
