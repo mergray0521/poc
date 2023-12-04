b443eea2dbb2a40681918d446c9ad728f265670c
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 08:47:04 -0500

    Update passport_app.py

--- passport_app.py
+++ passport_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('Welcome to Passport')
 streamlit.header('Create Token Schema')
 
-git clone https://github.com/snowflakedb/snowflake-connector-python.git
+git clone <https://github.com/snowflakedb/snowflake-connector-python.git>
 
