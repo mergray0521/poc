20eb61e4dfd82ab42880ac4ca9a3df24f452f6cc
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 12:25:06 -0500

    Update passport_app.py
    ""

--- passport_app.py
+++ passport_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute(select * from token_schemas)
+my_cur.execute("select * from token_schemas")
 my_data_row = my_cur.fetchone()
 streamlit.text("Current token schemas include:")
 streamlit.text(my_data_row)
