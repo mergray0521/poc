ee6facaf439f255e644cd8e94e6ff4ed0ba7d334
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 08:36:56 -0500

    Update passport_app.py
    cloning github

--- passport_app.py
+++ passport_app.py
@@ -3,15 +3,5 @@ import streamlit
 streamlit.title('Welcome to Passport')
 streamlit.header('Create Token Schema')
 
-import snowflake.connector
-
-[connections.snowflake]
-account = "CNAHFEW.KNB62637"
-user = "mergray0521"
-password = "Lakegage0521!"
-role = "accountadmin"
-warehouse = "compute_wh"
-database = "token_schemas"
-schema = "token_schemas"
-client_session_keep_alive = true
+git clone <https://github.com/snowflakedb/snowflake-connector-python.git>
 
