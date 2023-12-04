675d031e05e130e88367078052240935ca33254d
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 09:37:45 -0500

    Update passport_app.py

--- passport_app.py
+++ passport_app.py
@@ -3,3 +3,16 @@ import streamlit
 streamlit.title('Welcome to Passport')
 streamlit.header('Create Token Schema')
 
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
