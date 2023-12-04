9beb98fe78b6df04fdbc610a87c7e5934e0df10b
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-04 11:45:02 -0500

    Update passport_app.py

--- passport_app.py
+++ passport_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
 
 streamlit.title('My Passport')
 streamlit.header('Create Token Schema')
