2fa1f99babfcbecc7b1f8eea3dacfbe0b2609f63
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 10:56:12 -0500

    Update home_page.py
    "

--- home_page.py
+++ home_page.py
@@ -9,7 +9,7 @@ st.title('My Passport')
 st.sidebar.success("Select a page above.")
 
 if "my_input" not in st.session_state:
-        st.session_state["my_input:] = ""
+        st.session_state["my_input"] = ""
 
 my_input = st.text_input("Input a text here", st.session_state["my_input"])
 submit = st.button("submit")
