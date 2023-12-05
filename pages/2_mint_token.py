e2295755ae63fb6fdf070c501071958260afba99
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 12:03:18 -0500

    Update 2_mint_token.py

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -16,7 +16,7 @@ my_cur = my_cnx.cursor()
 
 my_cur.execute("select token_name from token_schemas")
 my_data_rows = my_cur.fetchall()
-st.header("Select Token Schema:")
+st.header("Current Token Schemas:")
 st.dataframe(my_data_rows)
 
 try:
