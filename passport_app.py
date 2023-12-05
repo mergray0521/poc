43e12380467e09fdc6ea8e0e49274fdba5ffcd23
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 09:48:00 -0500

    Update passport_app.py
    add home page

--- passport_app.py
+++ passport_app.py
@@ -4,7 +4,12 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
+st.set_page_config(
+    page_title="Create Token Schema"
+)
+
 st.title('Create Token Schema')
+st.sidebar.success("Select a page above.")
 
 def main():
     st.header("Token Information Form")
