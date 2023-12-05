e8049d48028b06f36cec9740f1efbfdb7ffba876
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 14:08:57 -0500

    Update 1_create_token_schema.py
    id

--- pages/1_create_token_schema.py
+++ pages/1_create_token_schema.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
 try:
         # Insert the form data into Snowflake
-        query = f"INSERT INTO token_schemas (token_name, fungibility, ip, token_admin, metadata) VALUES ('{token_name}', '{fungibility}', '{ip}', '{token_admin}', '{metadata}')"
+        query = f"INSERT INTO token_schemas (token_id, token_name, fungibility, ip, token_admin, metadata) VALUES ('{token_id}','{token_name}', '{fungibility}', '{ip}', '{token_admin}', '{metadata}')"
         my_cursor.execute(query)
         my_cnx.commit()
         st.success("Data successfully inserted into Snowflake database!")
