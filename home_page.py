0e17b4a58a3b7ac309ef5c398886392d9e43a2f1
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 11:17:58 -0500

    Update home_page.py
    try radio button

--- home_page.py
+++ home_page.py
@@ -14,11 +14,48 @@ user_pages = ["4_display_tokens.py", "5_earned_token.py", "6_update_metadata.py"
 
 if "my_input" not in st.session_state:
         st.session_state["my_input"] = ""
+if "role" not in st.session_state:
+    st.session_state.role = ""
 
-my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("submit")
-if submit:
-    st.session_state["my_input"] = my_input
-    st.write("You have entered: ", my_input)
+# Home Page
+def home_page():
+    st.title("Home Page")
+    st.header("Choose your role:")
 
+    # Radio button to choose role
+    role = st.radio("Select Role", ["Admin", "User"])
+
+    # Set session state role
+    get_session_state().role = role
+
+    # Continue button
+    if st.button("Continue"):
+        st.success(f"You selected {role} role. Proceed to {role} pages.")
+
+# Function to display pages based on role
+def display_pages():
+    role = get_session_state().role
+
+    if role == "Admin":
+        pages = admin_pages
+    elif role == "User":
+        pages = user_pages
+    else:
+        pages = []
+
+    return pages
+
+# Main function
+def main():
+    get_session_state()
+    home_page()
+
+    # Display pages based on role
+    pages = display_pages()
+    if pages:
+        selected_page = st.sidebar.selectbox("Select Page", pages)
+        st.write(f"You are viewing {selected_page}.")
+
+if __name__ == "__main__":
+    main()
 
