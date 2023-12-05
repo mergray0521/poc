0fc75addb84e989caec1659c96565f87c983f502
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 11:47:42 -0500

    Update 2_mint_token.py
    try choosing schema

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -8,8 +8,19 @@ my_cur = my_cnx.cursor()
 
 my_cur.execute("select token_name from token_schemas")
 my_data_rows = my_cur.fetchall()
-st.header("The current token schemas inclue:")
+st.header("The current token schemas include:")
 st.dataframe(my_data_rows)
+
+try:
+    schema_choice = streamlit.text_input('Which schema would you like to add a token to?')
+    if not schema_choice:
+        streamlit.error("Please select a token schema to proceed.")
+    else:
+        back_from_function = token_schema(schema_choice)
+        streamlit.dataframe(back_from_function)
+
+except URLError as e:
+  streamlit.error()
 
 
 
