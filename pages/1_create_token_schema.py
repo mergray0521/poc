340079a662866658093de6c121edd270edab3c25
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 16:54:34 -0500

    Update 1_create_token_schema.py

--- pages/1_create_token_schema.py
+++ pages/1_create_token_schema.py
@@ -14,7 +14,7 @@ def main():
     st.header("Token Information Form")
 
     # Create input fields for token information
-    token_schema_id = st.number_input("Token Schema ID", step= int, "")
+    token_schema_id = st.number_input("Token Schema ID", "")
     token_name = st.text_input("Token Name", "")
     fungibility = st.selectbox("Fungibility", ["Fungible", "Non-Fungible", "Semi-Fungible"])
     ip = st.selectbox("IP", ["HTTYD", "HHN", "CINEPHILE", "TOOTHSOME"])
