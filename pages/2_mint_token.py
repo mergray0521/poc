2d47d28ae351f7f57ee5f2471d5d9e6a9a1fee48
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 11:30:30 -0500

    Update 2_mint_token.py
    select token schema

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -2,3 +2,9 @@ import streamlit as st
 import snowflake.connector
 
 st.title("Mint Token")
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+def main():
+    st.header("Select Token Schema")
