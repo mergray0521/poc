4b07c79122802cb88ff218f6366530afcd1c5d19
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 10:55:30 -0500

    Update home_page.py
    test

--- home_page.py
+++ home_page.py
@@ -8,3 +8,11 @@ st.set_page_config(
 st.title('My Passport')
 st.sidebar.success("Select a page above.")
 
+if "my_input" not in st.session_state:
+        st.session_state["my_input:] = ""
+
+my_input = st.text_input("Input a text here", st.session_state["my_input"])
+submit = st.button("submit")
+if submit:
+    st.session_state["my_input"] = my_input
+    st.write("You have entered: ", my_input)
