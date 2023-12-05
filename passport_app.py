f9dd1ed2846863ba6ab3814ba972e368bd8d5e00
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 09:42:39 -0500

    Update passport_app.py
    back

--- passport_app.py
+++ passport_app.py
@@ -4,21 +4,9 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-# Function to create session state
-def get_session_state():
-    session_id = st.report_thread.get_report_ctx().session_id
-    session_state = st.session_state.get(session_id=session_id, page="home")
-    return session_state
+st.title('Create Token Schema')
 
-# Page 1: Home
-def home_page():
-    st.title('My Passport')
-    st.header("Home Page Content")
-    # Add content for the home page if needed
-
-# Page 2: Token Information Form
-def token_info_page():
-    st.title('Create Token Schema')
+def main():
     st.header("Token Information Form")
 
     # Create input fields for token information
@@ -44,22 +32,9 @@ def token_info_page():
         st.write("Result:")
         st.write(result)
 
-# Main function to switch between pages
-def main():
-    session_state = get_session_state()
-
-   # Navigation
-    pages = ["Home", "Token Information Form"]
-    selected_page = st.sidebar.radio("Navigation", pages)
-
-    # Display selected page
-    if selected_page == "Home":
-        home_page()
-    elif selected_page == "Token Information Form":
-        token_info_page()
-
 if __name__ == "__main__":
     main()
+
 
 sys.exit()
 
