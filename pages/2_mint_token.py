dbfd3046f11093f375434819ce7c08d5d47f1ab9
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-05 11:44:31 -0500

    Update 2_mint_token.py
    just one column

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -6,9 +6,10 @@ st.title("Mint Token")
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
-my_cur.execute("select * from token_schemas")
+my_cur.execute("select token_name from token_schemas")
 my_data_rows = my_cur.fetchall()
-st.header("Select Token Schema")
+st.header("The current token schemas inclue:")
 st.dataframe(my_data_rows)
+
 
 
