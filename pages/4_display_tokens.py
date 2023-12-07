7eae65f2d56bec170ef1431f42b623c3ea83c72c
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 11:32:59 -0500

    Update 4_display_tokens.py
    added if

--- pages/4_display_tokens.py
+++ pages/4_display_tokens.py
@@ -5,22 +5,12 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**st.secrets["TOKEN_OWNERSHIP"])
 my_cur = my_cnx.cursor()
 
-
 st.title("Display Tokens")
 
 # Input user ID
 user_id = st.text_input("Enter User ID:")
 
 if user_id:
-    # Connect to Snowflake
-    conn = snowflake.connector.connect(
-        user=snowflake_username,
-        password=snowflake_password,
-        account=snowflake_account,
-        warehouse=snowflake_warehouse,
-        database=snowflake_database,
-        role=snowflake_role
-    )
 
     # Query the token_ownership table
     query = f"SELECT token_id FROM token_ownership WHERE owner_id = '{user_id}'"
