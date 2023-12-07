1fcd7415928b4a854500f2275e78d616650d9b27
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 12:39:56 -0500

    Update 2_mint_token.py
    try simple mint

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -2,39 +2,35 @@ import streamlit as st
 import snowflake.connector
 import pandas as pd
 
-def fetch_token_columns(token_schema):
-    query = f"DESCRIBE TABLE {token_schema}"
-    my_cur.execute(query)
-    result_data = my_cur.fetchall()
-    return [column[0] for column in result_data]
-
 st.title("Select Token Schema")
-
-my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
-my_cur = my_cnx.cursor()
 
 token_schema_options = ["avatar wearables", "dragon egg", "egg feathers", "egg nests", "healing herbs", "sketchbook", "star maps", "trained dragon", "weapons"]
 selected_token_schema = st.selectbox("Token Schema", token_schema_options)
 
-if selected_token_schema == "avatar wearables" and st.button("Submit"):
-    # Fetch columns for the selected table
-    columns = fetch_token_columns(selected_token_schema)
+if st.button("Submit"):
+    my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
+    my_cur = my_cnx.cursor()
 
-    # Display the columns for the selected table
-    st.success(f"Columns for {selected_token_schema}:")
-    st.write(columns)
+def main():
+    st.header("Create New Token")
 
-    # Placeholder for the dynamic query using the selected token_schema
-    # Replace this with your actual logic to retrieve data based on the selected token_schema
-    query = f"SELECT * FROM {selected_token_schema}"
-    my_cur.execute(query)
-    result = my_cur.fetchall()
+    # Create input fields for token information
+    token_id = st.number_input("Token ID", min_value=606, max_value=1000, value=606, step=1)
+    type = st.text_input("Token Type", "")
+    materials = st.text_input("Materials", "")
+    color = st.selectbox("IP", ["Green", "Black", "Silver", "Red", "Brown"])
 
-    # Display the result in Streamlit
-    if result:
-        st.success(f"Data for {selected_token_schema}:")
-        tokens_df = pd.DataFrame(result, columns=columns)
-        st.dataframe(tokens_df)
+    # Submit button
+    if st.button("Submit"):
+        
+        # Insert the form data into Snowflake
+        query = f"INSERT INTO avatar_wearables (TOKEN_ID,TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{type}', '{materials}', '{color}')"
+        my_cur.execute(query)
+        my_cnx.commit()
+        st.success("New token minted!")
+
+if __name__ == "__main__":
+    main()
 
 
 
