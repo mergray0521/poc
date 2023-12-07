5e4d403c4e9f668df9aa7ac43988208525d9b868
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 10:12:40 -0500

    Update 2_mint_token.py
    hard code options

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -14,7 +14,7 @@ def token_schemas():
     return result_data
 
 # Call the function to get schema options
-schema_options = "SELECT * FROM inventory_db.{schema_name}"
+schema_options = st.selectbox("Token Schemas", ["dragon_egg", "egg_nests", "egg_feathers", "trained_dragon", "sketchbook", "avatar_wearables", "weapons", "healing_herbs", "star_maps", "points"] )
 
 # Create dropdown
 selected_schema = st.selectbox("Select Token Schema", [schema[0] for schema in schema_options])
