81b9b602cf9276f501915a963122d077d6550a1a
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 13:17:22 -0500

    Update 3_assign_ownership.py
    put main first

--- pages/3_assign_ownership.py
+++ pages/3_assign_ownership.py
@@ -1,13 +1,27 @@
 import streamlit as st
 import snowflake.connector
 
+def main():
+    st.title("Assign Token Ownership")
+
+    # Get user input
+    token_id = st.text_input("Token ID:")
+    owner_id = st.text_input("Owner ID:")
+    quantity = st.text_input("Quantity:")
+
+    # Check if all fields are filled
+    if token_id and owner_id and quantity:
+        if st.button("Assign Ownership"):
+            # Call the function to update ownership in Snowflake
+            update_ownership(token_id, owner_id, quantity)
+
 # Function to update ownership in Snowflake
 def update_ownership(token_id, owner_id, quantity):
     try:
-
-        my_cnx = snowflake.connector.connect(**st.secrets["TOKEN_OWNERSHIP"])
+        # Establish Snowflake connection
+        my_cnx = snowflake.connector.connect(**snowflake_connection_params)
         my_cur = my_cnx.cursor()
-        
+
         # Your SQL query to update the ownership table
         query = f"UPDATE TOKEN_OWNERSHIP SET OWNER_ID = '{owner_id}', Quantity = {quantity} WHERE TOKEN_ID = '{token_id}'"
 
@@ -26,21 +40,6 @@ def update_ownership(token_id, owner_id, quantity):
         # Close the connection
         if my_cnx:
             my_cnx.close()
-
-# Streamlit app
-def main():
-    st.title("Assign Token Ownership")
-
-    # Get user input
-    token_id = st.text_input("Token ID:")
-    owner_id = st.text_input("Owner ID:")
-    quantity = st.text_input("Quantity:")
-
-    # Check if all fields are filled
-    if token_id and owner_id and quantity:
-        if st.button("Assign Ownership"):
-            # Call the function to update ownership in Snowflake
-            update_ownership(token_id, owner_id, quantity)
 
 # Run the app
 if __name__ == "__main__":
