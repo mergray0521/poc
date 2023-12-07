89f817df1fcd6122b2c0ccadd024cf21ce7889ce
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 12:25:03 -0500

    Update 2_mint_token.py
    try my cur inside

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import snowflake.connector
+import pandas as pd
 
 def fetch_token_columns(token_schemas):
     columns_dict = {}
@@ -15,36 +16,33 @@ def fetch_token_columns(token_schemas):
 
 st.title("Select Token Schema")
 
-my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
-my_cur = my_cnx.cursor()
-
-token_schema_options = ["avatar wearables", "dragon egg", "egg feathers", "egg nests", "healing herbs", "sketchbook", "star maps", "trained dragon", "weapons"]
-token_schema = st.selectbox("Token Schema", token_schema_options)
-
 if st.button("Submit"):
-  if selected_token_schema:
+    my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
+    my_cur = my_cnx.cursor()
+
+    token_schema_options = ["avatar wearables", "dragon egg", "egg feathers", "egg nests", "healing herbs", "sketchbook", "star maps", "trained dragon", "weapons"]
+    selected_token_schema = st.selectbox("Token Schema", token_schema_options)
+
+    if selected_token_schema:
         # Fetch columns for the selected table
         columns_dict = fetch_token_columns([selected_token_schema])
 
-      # Display the columns for the selected table
+        # Display the columns for the selected table
         for token_schema, columns in columns_dict.items():
-            st.success(f"Columns for {avatar_wearables}:")
+            st.success(f"Columns for {token_schema}:")
             st.write(columns)
-    
+
             # Placeholder for the dynamic query using the selected token_schema
             # Replace this with your actual logic to retrieve data based on the selected token_schema
             query = f"SELECT * FROM {token_schema}"
             my_cur.execute(query)
             result = my_cur.fetchall()
-    
+
             # Display the result in Streamlit
             if result:
                 st.success(f"Data for {token_schema}:")
                 tokens_df = pd.DataFrame(result, columns=columns)
                 st.dataframe(tokens_df)
-
-
-
 
 
 
