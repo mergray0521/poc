643bba01f539c9288dafd8e79c636df3aa9831ac
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 09:56:44 -0500

    Update 2_mint_token.py
    no ident

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -10,8 +10,8 @@ my_cur = my_cnx.cursor()
 # Query token schema options
 schema_options = f"SELECT * FROM inventory_db.{schema_name}"
 my_cur.execute(query)
-    result_data = my_cur.fetchall()
-    return result_data
+result_data = my_cur.fetchall()
+return result_data
 
 selected_schema = st.selectbox("Select Token Schema", [schema[0] for schema in schema_options])
 
