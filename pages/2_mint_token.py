eadfc6e08c685c00bbd1b4a0ed1f7fac67202600
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 09:53:58 -0500

    Update 2_mint_token.py
    re-do page

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -2,33 +2,19 @@ import streamlit as st
 import snowflake.connector
 from urllib.error import URLError
 
-def token_schemas(schema_name):
-    # Assume "inventory_db" is the name of the different database
-    query = f"SELECT * FROM inventory_db.{schema_name}"
-    my_cur.execute(query)
+st.title("Mint Token")
+
+my_cnx = snowflake.connector.connect(**st.secrets["token_schemas", "inventory_db"])
+my_cur = my_cnx.cursor()
+
+# Query token schema options
+schema_options = f"SELECT * FROM inventory_db.{schema_name}"
+my_cur.execute(query)
     result_data = my_cur.fetchall()
     return result_data
 
-st.title("Mint Token")
+selected_schema = st.selectbox("Select Token Schema", [schema[0] for schema in schema_options])
 
-my_cnx = snowflake.connector.connect(**st.secrets["token_schemas"])
-my_cur = my_cnx.cursor()
-
-my_cur.execute("select token_name from token_schemas")
-my_data_rows = my_cur.fetchall()
-st.header("Current Token Schemas:")
-st.dataframe(my_data_rows)
-
-try:
-    schema_choice = st.text_input('Which schema would you like to add a token to?')
-    if not schema_choice:
-        st.error("Please select a token schema to proceed.")
-    else:
-        back_from_function = token_schemas(schema_choice)
-        st.dataframe(back_from_function)
-
-except URLError as e:
-  st.error()
 
 
 
