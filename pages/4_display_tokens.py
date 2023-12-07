21ce432ab2df89908a6c86b7d7ca168b9db68638
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 11:38:18 -0500

    Update 4_display_tokens.py
    added button

--- pages/4_display_tokens.py
+++ pages/4_display_tokens.py
@@ -10,22 +10,23 @@ st.title("Display Tokens")
 # Input user ID
 user_id = st.text_input("Enter User ID:")
 
-if user_id:
-
-    # Query the token_ownership table
-    query = f"SELECT token_id FROM token_ownership WHERE owner_id = '{user_id}'"
-    my_cur.execute(query)
-    result = cursor.fetchall()
-
-    # Display the result in Streamlit
-    if result:
-        st.success(f"Tokens for User ID {user_id}:")
-        tokens_df = pd.DataFrame(result, columns=["Token ID"])
-        st.dataframe(tokens_df)
-    else:
-        st.warning(f"No tokens found for User ID {user_id}")
-
-    # Close Snowflake connection
-    conn.close()
+if st.button("Submit"):
+    if user_id:
+    
+        # Query the token_ownership table
+        query = f"SELECT token_id FROM token_ownership WHERE owner_id = '{user_id}'"
+        my_cur.execute(query)
+        result = cursor.fetchall()
+    
+        # Display the result in Streamlit
+        if result:
+            st.success(f"Tokens for User ID {user_id}:")
+            tokens_df = pd.DataFrame(result, columns=["Token ID"])
+            st.dataframe(tokens_df)
+        else:
+            st.warning(f"No tokens found for User ID {user_id}")
+    
+        # Close Snowflake connection
+        conn.close()
 
 
