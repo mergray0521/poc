1298fe17dd4bf1a9a30d1bf01a0d9e61118c1f08
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 13:15:20 -0500

    Update 3_assign_ownership.py
    try this

--- pages/3_assign_ownership.py
+++ pages/3_assign_ownership.py
@@ -6,26 +6,44 @@ my_cur = my_cnx.cursor()
 
 st.title("Assign Token Ownership")
 
-# Get user input
-token_id = st.text_input("Token ID:")
-owner_id = st.text_input("Owner ID:")
-quantity = st.text_input("Quantity:")
+# Function to update ownership in Snowflake
+def update_ownership(token_id, owner_id, quantity):
+    try:
 
-# Check if all fields are filled
-if token_id and owner_id and quantity:
-    if st.button("Assign Ownership"):
-        try:
-            # Your SQL query to update the ownership table
-            query = f"UPDATE TOKEN_OWNERSHIP SET OWNER_ID = '{owner_id}', Quantity = {quantity} WHERE TOKEN_ID = '{token_id}'"
-            
-            # Execute the query
-            my_cur.execute(query)
+        # Your SQL query to update the ownership table
+        query = f"UPDATE TOKEN_OWNERSHIP SET OWNER_ID = '{owner_id}', Quantity = {quantity} WHERE TOKEN_ID = '{token_id}'"
 
-            # Commit the changes
-            my_cnx.commit()
+        # Execute the query
+        my_cur.execute(query)
 
-            st.success("Ownership updated successfully!")
+        # Commit the changes
+        my_cnx.commit()
 
-        except Exception as e:
-            st.error(f"Error updating ownership: {str(e)}")
+        st.success("Ownership updated successfully!")
 
+    except Exception as e:
+        st.error(f"Error updating ownership: {str(e)}")
+
+    finally:
+        # Close the connection
+        if my_cnx:
+            my_cnx.close()
+
+# Streamlit app
+def main():
+    st.title("Assign Token Ownership")
+
+    # Get user input
+    token_id = st.text_input("Token ID:")
+    owner_id = st.text_input("Owner ID:")
+    quantity = st.text_input("Quantity:")
+
+    # Check if all fields are filled
+    if token_id and owner_id and quantity:
+        if st.button("Assign Ownership"):
+            # Call the function to update ownership in Snowflake
+            update_ownership(token_id, owner_id, quantity)
+
+# Run the app
+if __name__ == "__main__":
+    main()
