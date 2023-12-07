006ee90697ecd4e546ed8b5bef5e1759d6dae6bb
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-07 10:59:31 -0500

    Update 2_mint_token.py
    inventory caps

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -3,7 +3,7 @@ import snowflake.connector
 
 st.title("Mint Token")
 
-my_cnx = snowflake.connector.connect(**st.secrets["inventory_db"])
+my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
 my_cur = my_cnx.cursor()
 
 def fetch_token_schemas():
@@ -18,3 +18,6 @@ schema_options = fetch_token_schemas()
 # Create dropdown
 selected_schema = st.selectbox("Select Token Schema", schema_options)
 
+if __name__ == "__main__":
+    main()
+        
