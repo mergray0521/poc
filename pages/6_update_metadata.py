ffdf102038d5983ad37039bd6b53e75870e56cd2
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-11 11:49:34 -0500

    Update 6_update_metadata.py

--- pages/6_update_metadata.py
+++ pages/6_update_metadata.py
@@ -1,15 +1,54 @@
 import streamlit as st
 import snowflake.connector
-import pandas as pd
 
 # Snowflake connection
 my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
 my_cur = my_cnx.cursor()
 
-# Load data from Snowflake
-query = "SELECT * FROM avatar_wearables"
-my_cur.execute(query)
-columns = [col[0] for col in my_cur.description]
-rows = my_cur.fetchall()
-df = pd.DataFrame(rows, columns=columns)
+# Token information (you can replace this with your Streamlit form input)
+token_id = 123
+type = "Example Token"
+materials = "Fungible"
+color = "HTTYD"
+
+# MERGE statement
+merge_query = f'''
+MERGE INTO avatar_wearables AS target
+USING (SELECT {token_id} AS "TOKEN_ID",
+              '{type}' AS "TYPE",
+              '{materials}' AS "MATERIALS",
+              '{color}' AS "COLOR") AS source
+ON target."token_id" = avatar_wearables."token_id"
+WHEN MATCHED THEN
+  UPDATE SET "TOKEN_ID" = avatar_wearables."TOKEN_NAME",
+             "TYPE" = avatar_wearables."TYPE",
+             "MATERIALS" = avatar_wearables."MATERIALS",
+             "COLOR" = avatar_wearables."COLOR",
+WHEN NOT MATCHED THEN
+  INSERT ("TOKEN_ID", "TYPE", "MATERIALS", "COLOR")
+  VALUES (avatar_wearables."TOKEN_ID", avatar_wearables."TYPE", avatar_wearables."MATERIALS", avatar_wearables."COLOR" );
+'''
+
+# Execute the MERGE query
+my_cur.execute(merge_query)
+my_cnx.commit()
+
+st.success("Table updated using MERGE statement")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
