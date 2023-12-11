624f1034992395c00ee849be242896f5c21c9e4d
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-11 12:05:45 -0500

    Update 6_update_metadata.py

--- pages/6_update_metadata.py
+++ pages/6_update_metadata.py
@@ -4,13 +4,8 @@ import streamlit as st
 import time
 from snowflake.snowpark import Session
 
-import pandas as pd
-import json
-import streamlit as st
-from snowflake.snowpark import Session
-import time
-
-xx
+my_cnx = snowflake.connector.connect(**st.secrets["token_schemas"])
+my_cur = my_cnx.cursor()
 
 st.set_page_config(layout="centered", page_title="Data Editor", page_icon="🧮")
 st.title("Snowflake Table Editor ❄️")
@@ -18,7 +13,7 @@ st.caption("This is a demo of the `st.experimental_data_editor`.")
 
 def get_dataset():
     # load messages df
-    df = session.table("ESG_SCORES_DEMO")
+    df = my_cur.table("AVATAR_WEARABLES")
 
     return df
 
@@ -32,14 +27,13 @@ with st.form("data_editor_form"):
 if submit_button:
     try:
         #Note the quote_identifiers argument for case insensitivity
-        session.write_pandas(edited, "ESG_SCORES_DEMO", overwrite=True, quote_identifiers=False)
+        session.write_pandas(edited, "AVATAR_WEARABLES", overwrite=True, quote_identifiers=False)
         st.success("Table updated")
         time.sleep(5)
     except:
         st.warning("Error updating table")
     #display success message for 5 seconds and update the table to reflect what is in Snowflake
     st.experimental_rerun()
-
 
 
 
