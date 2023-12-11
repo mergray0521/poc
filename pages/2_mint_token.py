b8fc2d0d690f56fe5e9e374844fb4a9e4e259f38
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-11 15:25:12 -0500

    Update 2_mint_token.py
    indent

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -15,22 +15,22 @@ def main():
         my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
         my_cur = my_cnx.cursor()
 
-        st.header("Create New Token")
+    st.header("Create New Token")
             
-        # Create form for token information
-        with st.form("mint_token"):
+    # Create form for token information
+    if st.form("mint_token"):
             token_id = st.number_input('Token ID"', min_value=606, max_value=1000, value=606, step=1)
             type = st.text_input('Token Type', "")
             materials = st.text_input('Materials', "")
             color = st.selectbox('Color', ["Green", "Black", "Silver", "Red", "Brown"]) 
             mint = st.form_submit_button('Mint')
       
-    if st.form_submit_button:            
-        # Insert the form data into Snowflake
-        query = f"INSERT INTO avatar_wearables (TOKEN_ID, TYPE, MATERIALS, COLOR) VALUES ('{token_id}', '{type}', '{materials}', '{color}')"
-        my_cur.execute(query)
-        my_cnx.commit()
-        st.success("New token minted!")
+        if st.form_submit_button:            
+            # Insert the form data into Snowflake
+            query = f"INSERT INTO avatar_wearables (TOKEN_ID, TYPE, MATERIALS, COLOR) VALUES ('{token_id}', '{type}', '{materials}', '{color}')"
+            my_cur.execute(query)
+            my_cnx.commit()
+            st.success("New token minted!")
             
 if __name__ == "__main__":
     main()
