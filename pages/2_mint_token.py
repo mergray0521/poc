dd415ea95827fbe96d24288a7f61b0a3fa8c8fdb
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-11 15:13:08 -0500

    Update 2_mint_token.py
    move mint

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -17,24 +17,21 @@ def main():
             
         # Create form for token information
         with st.form("mint_token"):
-            token_id = st.number_input('Token ID', min_value=606, max_value=1000, value=606, step=1)
+            token_id = st.number_input('Token ID"', min_value=606, max_value=1000, value=606, step=1)
             type = st.text_input('Token Type', "")
             materials = st.text_input('Materials', "")
             color = st.selectbox('Color', ["Green", "Black", "Silver", "Red", "Brown"]) 
             mint = st.form_submit_button('Mint')
       
-         
-            if mint:        
-                # Insert the form data into Snowflake
-                query = f"INSERT INTO avatar_wearables (TOKEN_ID,TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{type}', '{materials}', '{color}')"
-                my_cur.execute(query)
-                my_cnx.commit()
-                st.success("New token minted!")
-                
+        if mint:           
+            # Insert the form data into Snowflake
+            query = f"INSERT INTO avatar_wearables (TOKEN_ID, TYPE, MATERIALS, COLOR) VALUES ('{token_id}', '{type}', '{materials}', '{color}')"
+            my_cur.execute(query)
+            my_cnx.commit()
+            st.success("New token minted!")
+            
 if __name__ == "__main__":
     main()
-            
-    
-    
+
     
     
