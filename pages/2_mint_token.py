8f79a3626b57275d2f909ac46200a7e9bdcf6359
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-11 15:42:26 -0500

    Update 2_mint_token.py
    mint btn

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -21,18 +21,18 @@ def main():
             type = st.text_input('Token Type', "")
             materials = st.text_input('Materials', "")
             color = st.selectbox('Color', ["Green", "Black", "Silver", "Red", "Brown"]) 
-            mint = st.form_submit_button('Mint')
+            mint_button_clicked = st.form_submit_button('Mint')
       
-         
-        if mint:           
+        if mint_button_clicked:           
             # Insert the form data into Snowflake
-            query = f"INSERT INTO avatar_wearables (TOKEN_ID,TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{type}', '{materials}', '{color}')"
+            query = f"INSERT INTO avatar_wearables (TOKEN_ID, TYPE, MATERIALS, COLOR) VALUES ('{token_id}', '{type}', '{materials}', '{color}')"
             my_cur.execute(query)
             my_cnx.commit()
             st.success("New token minted!")
             
 if __name__ == "__main__":
     main()
+
 
 
 
