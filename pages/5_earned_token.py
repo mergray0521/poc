37207bf3694854eefd91c9f8dcb677d0ae22d606
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-11 12:36:59 -0500

    Update 5_earned_token.py
    collect token

--- pages/5_earned_token.py
+++ pages/5_earned_token.py
@@ -2,3 +2,16 @@ import streamlit as st
 import snowflake.connector
 
 st.title("Earned Token")
+
+
+
+
+
+  if st.button("Collect"):
+                    
+            # Insert the form data into Snowflake
+            query = f"INSERT INTO avatar_wearables (TOKEN_ID,TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{type}', '{materials}', '{color}')"
+            my_cur.execute(query)
+            my_cnx.commit()
+            st.success("New token collected!")
+            
