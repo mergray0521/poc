95aac05190a93f0d09443ac692c3d47a6658540c
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-11 10:12:57 -0500

    Update 6_update_metadata.py
    update based on expiring

--- pages/6_update_metadata.py
+++ pages/6_update_metadata.py
@@ -9,8 +9,7 @@ my_cur = my_cnx.cursor()
 
 # Streamlit configuration
 st.set_page_config(layout="centered", page_title="Data Editor", page_icon="🧮")
-st.title("Snowflake Table Editor ❄️")
-st.caption("This is a demo of the `st.experimental_data_editor`.")
+st.title("Update Metadata")
 
 # Function to get dataset from Snowflake
 def get_dataset():
@@ -26,7 +25,7 @@ dataset = get_dataset()
 # Display the editable data editor form
 with st.form("data_editor_form"):
     st.caption("Edit the dataframe below")
-    edited = st.experimental_data_editor(dataset, use_container_width=True, num_rows="dynamic")
+    edited = st.data_editor(dataset, use_container_width=True)
     submit_button = st.form_submit_button("Submit")
 
 # Upon submitting changes
@@ -41,6 +40,3 @@ if submit_button:
         st.success("Table updated")
     except Exception as e:
         st.warning(f"Error updating table: {e}")
-
-    # Update the table to reflect changes in Snowflake
-    st.experimental_rerun()
