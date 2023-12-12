523dca017d43b50f581f9e17424b61ce1469f825
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 09:57:52 -0500

    Update 2_mint_token.py
    get session state

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import snowflake.connector
 
+# Function to get or create session state
+@st.cache(allow_output_mutation=True)
+def get_session_state():
+    return {"form_1_submitted": False, "form_2_submitted": False}
+
 def main():
     st.title("Mint Token")
+
+    # Get or create session state
+    session_state = get_session_state()
 
     # form 1 for token schema selection
     with st.form("Token Schema"):
@@ -11,9 +19,11 @@ def main():
         search = st.form_submit_button("Search")
 
     if search:
+        session_state["form_1_submitted"] = True
         st.success(f"Form 1 submitted with input: {token_schema}")
 
-        # form 2 for token information
+    # form 2 for token information
+    if session_state["form_1_submitted"]:
         with st.form("Mint Token"):
             token_id = st.number_input('Token ID', min_value=606, max_value=1000, value=606, step=1)
             token_type = st.text_input('Token Type', "")
@@ -22,7 +32,7 @@ def main():
 
             # Check for form 2 submission
             if st.form_submit_button("Mint"):
-                # Connect to Snowflake within the form 2 block
+                # Connect to Snowflake
                 my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
                 my_cur = my_cnx.cursor()
 
