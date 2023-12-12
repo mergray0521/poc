ba462be4bd11721963938425c3fb2237ba3a20ef
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:48:59 -0500

    Update 7_marketplace.py
    300

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -11,7 +11,7 @@ token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=Img
 col1, col2, col3 = st.columns(3)
 
 # Display images in columns
-col1.image(token_1, caption="My Say Token", use_column_width=True, width=200)
+col1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
 col2.image(token_2, caption="My Way Token", use_column_width=True, width=100)
 col3.image(token_3, caption="My Day Token", use_column_width=True, width=100)
 
