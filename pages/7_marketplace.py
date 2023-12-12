d6c27dfad5b4f64a84e381da2ddf8b6c0af66c00
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:34:18 -0500

    Update 7_marketplace.py
    raw string

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -4,6 +4,6 @@ import snowflake.connector
 st.title("Marketplace")
 
 token_1 = "https://insidethemagic.net/wp-content/uploads/2017/05/Screen-Shot-2017-05-05-at-3.44.38-PM.jpg"
-token_2 = "C:\Users\meredith.gray\Downloads\Token1.png"
+token_2 = r"C:\Users\meredith.gray\Downloads\Token1.png"
 st.image(token_2, caption="Token: X", use_column_width=True)
 
