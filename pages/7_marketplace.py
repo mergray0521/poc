37fb769660b5c0e1f5a89c193172504cd7e92fcb
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:35:10 -0500

    Update 7_marketplace.py
    width

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -5,5 +5,5 @@ st.title("Marketplace")
 
 token_1 = "https://insidethemagic.net/wp-content/uploads/2017/05/Screen-Shot-2017-05-05-at-3.44.38-PM.jpg"
 token_2 = r"C:\Users\meredith.gray\Downloads\Token1.png"
-st.image(token_2, caption="Token: X", use_column_width=True)
+st.image(token_2, caption="Token: X", use_column_width=True, width=200)
 
