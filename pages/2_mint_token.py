82e3554a066bddf3a08a7b8dc064f32711106c70
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 09:48:54 -0500

    Update 2_mint_token.py

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -11,7 +11,7 @@ def main():
     my_cur = my_cnx.cursor()
 
     # form 1 for token schema selection
-    with st.form ("Token Schema"):
+    with st.form("Token Schema"):
         st.header("Token Schema")
         token_schema = st.selectbox('Token Schema', ["avatar wearables", "dragon egg", "egg feathers", "egg nests", "healing herbs", "sketchbook", "star maps", "trained dragon", "weapons"])
         search = st.form_submit_button("Search")
@@ -22,17 +22,17 @@ def main():
         # form 2 for token information
         with st.form("Mint Token"):
             token_id = st.number_input('Token ID', min_value=606, max_value=1000, value=606, step=1)
-            type = st.text_input('Token Type', "")
+            token_type = st.text_input('Token Type', "")
             materials = st.text_input('Materials', "")
             color = st.selectbox('Color', ["Green", "Black", "Silver", "Red", "Brown"]) 
             mint = st.form_submit_button("Mint")
 
             if mint:
                 # Insert the form data into Snowflake
-                query = f"INSERT INTO avatar_wearables (TOKEN_ID,TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{type}', '{materials}', '{color}')"
+                query = f"INSERT INTO avatar_wearables (TOKEN_ID, TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{token_type}', '{materials}', '{color}')"
                 my_cur.execute(query)
                 my_cnx.commit()
-                st.success(f"Form 1 submitted with input: {token_id, type, materials, color}")
+                st.success(f"Form 2 submitted with input: {token_id, token_type, materials, color}")
             
 if __name__ == "__main__":
     main()
