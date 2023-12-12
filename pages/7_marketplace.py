cb3066c62fcf943777ad08ebd7ec0d769b3a9b3e
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:44:55 -0500

    Update 7_marketplace.py
    test 3

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -3,7 +3,11 @@ import snowflake.connector
 
 st.title("Marketplace")
 
-token_1 = "https://insidethemagic.net/wp-content/uploads/2017/05/Screen-Shot-2017-05-05-at-3.44.38-PM.jpg"
-token_2 = r"C:\Users\meredith.gray\Downloads\Token1.png"
+token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
+token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
+token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain"
+
+st.image(token_1, caption="Token: X", use_column_width=True, width=200)
 st.image(token_2, caption="Token: X", use_column_width=True, width=200)
+st.image(token_3, caption="Token: X", use_column_width=True, width=200)
 
