75efb200cbf604048d93353f35675bd8faf287e0
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 14:24:54 -0500

    Update 5_earned_token.py
    3

--- pages/5_earned_token.py
+++ pages/5_earned_token.py
@@ -6,19 +6,17 @@ my_cnx = snowflake.connector.connect(**st.secrets["token_schemas"])
 my_cur = my_cnx.cursor()
 
 st.header("Earned Token") 
-st.text ("Congratulations! You earned a new Flying Harness token!")
+st.text("Congratulations! You earned a new Flying Harness token!")
 
 image_url = "https://insidethemagic.net/wp-content/uploads/2017/05/Screen-Shot-2017-05-05-at-3.44.38-PM.jpg"
 st.image(image_url, caption="Token: Flying Harness", use_column_width=True)
 
 if st.button("Collect"):
-# Insert the form data into Snowflake
-        query = f"INSERT INTO token_ownership (TOKEN_ID, OWNER_ID, QUANTITY) VALUES ('{605}','{3}', '{1}')"
-        my_cur.execute(query)
-        my_cnx.commit()
-        st.success(f"Flying harness token added to user: {user_id}'s wallet"
-
-        
+    # Insert the form data into Snowflake
+    query = f"INSERT INTO token_ownership (TOKEN_ID, OWNER_ID, QUANTITY) VALUES ('{605}','{3}', '{1}')"
+    my_cur.execute(query)
+    my_cnx.commit()
+    st.success(f"Flying harness token added to user: {3}'s wallet")
 
 
 
