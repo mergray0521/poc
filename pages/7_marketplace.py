3eeec8e143de590d042c4680ff0cb9ae2805fe42
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:49:23 -0500

    Update 7_marketplace.py
    50

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -12,7 +12,7 @@ col1, col2, col3 = st.columns(3)
 
 # Display images in columns
 col1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
-col2.image(token_2, caption="My Way Token", use_column_width=True, width=100)
-col3.image(token_3, caption="My Day Token", use_column_width=True, width=100)
+col2.image(token_2, caption="My Way Token", use_column_width=True, width=50)
+col3.image(token_3, caption="My Day Token", use_column_width=True, width=50)
 
 
