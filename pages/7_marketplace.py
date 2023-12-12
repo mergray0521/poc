14c56d32426f1abde3942578456059cd48953a2b
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:32:42 -0500

    Update 7_marketplace.py
    token

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -2,3 +2,8 @@ import streamlit as st
 import snowflake.connector
 
 st.title("Marketplace")
+
+token_1 = "https://insidethemagic.net/wp-content/uploads/2017/05/Screen-Shot-2017-05-05-at-3.44.38-PM.jpg"
+token_2 = "C:\Users\meredith.gray\Downloads\Token1.png"
+st.image(token_2, caption="Token: X", use_column_width=True)
+
