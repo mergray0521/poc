3123907a5fab42cb80f223d20a2faf05020accf5
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:48:23 -0500

    Update 7_marketplace.py
    caption

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.header("Token Marketplace")
+
 # Sample image URLs
 token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
 token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
@@ -9,8 +11,8 @@ token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=Img
 col1, col2, col3 = st.columns(3)
 
 # Display images in columns
-col1.image(token_1, caption="Token: X", use_column_width=True, width=100)
-col2.image(token_2, caption="Token: X", use_column_width=True, width=100)
-col3.image(token_3, caption="Token: X", use_column_width=True, width=100)
+col1.image(token_1, caption="My Say Token", use_column_width=True, width=200)
+col2.image(token_2, caption="My Way Token", use_column_width=True, width=100)
+col3.image(token_3, caption="My Day Token", use_column_width=True, width=100)
 
 
