cfae705cb3f2190a7bb7afec77c5d5c9410fd606
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 15:31:08 -0500

    Update 4_display_tokens.py
    back to old v

--- pages/4_display_tokens.py
+++ pages/4_display_tokens.py
@@ -12,32 +12,18 @@ user_id = st.text_input("Enter User ID:")
 
 if st.button("Submit"):
     if user_id:
-
-        # Query the token_ownership table and join with token_images 
-        query = f"""
-            SELECT to.token_id, ti.image_url
-            FROM token_ownership to
-            JOIN token_images ti ON to.token_id = ti.token_id
-            WHERE to.owner_id = '{user_id}'
-        """
+    
+        # Query the token_ownership table
+        query = f"SELECT token_id FROM token_ownership WHERE owner_id = '{user_id}'"
         my_cur.execute(query)
         result = my_cur.fetchall()
     
-        
+        # Display the result in Streamlit
         if result:
             st.success(f"Tokens for User ID {user_id}:")
-        
-            # Create a three-column layout
-            col1, col2, col3 = st.columns(3)
-        
-            for token_id, image_url in result:
-                # Display image and caption in each column
-                with col1:
-                    st.image(image_url, caption=f"Token ID: {token_id}", use_column_width=True)
-                # Repeat for col2 and col3
+            tokens_df = pd.DataFrame(result, columns=["Token ID"])
+            st.dataframe(tokens_df)
         else:
             st.warning(f"No tokens found for User ID {user_id}")
-            
     
-
 
