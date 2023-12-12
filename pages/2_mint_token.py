008378b1e86a88f83256140fbecfcdb1c0c265d0
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 10:38:39 -0500

    Update 2_mint_token.py
    try

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import snowflake.connector
 
-st.title("Test")
-with st.form("form-1"):
-    submitted_1 = st.form_submit_button(label="Submit")
-    if not submitted_1 and not st.session_state.get("submission-1"):
+st.title("Mint Token")
+with st.form("token_schema"):
+    st.header("Token Schema")
+    token_schema = st.selectbox('Token Schema', ["avatar wearables", "dragon egg", "egg feathers", "egg nests", "healing herbs", "sketchbook", "star maps", "trained dragon", "weapons"])
+    search = st.form_submit_button(label="Search")
+    if not search and not st.session_state.get("Search"):
         st.stop()
-    st.session_state["submission-1"] = True
+    st.session_state["Search"] = True
     st.write("1")
-with st.form("form-2"):
-    submitted_2 = st.form_submit_button(label="Submit")
-    if not submitted_2:
+
+
+with st.form("mint_token"):
+    mint = st.form_submit_button(label="Mint")
+    if not mint:
         st.stop()
-    st.session_state["submission-1"] = False
+    st.session_state["Mint"] = False
     st.write("2")
