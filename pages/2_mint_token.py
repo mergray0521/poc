85e0f64f78daba23e8d724ffb0c66ca293a32578
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 09:55:00 -0500

    Update 2_mint_token.py
    put conenction in mint

--- pages/2_mint_token.py
+++ pages/2_mint_token.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import snowflake.connector
-import pandas as pd
 
 def main():
-    
     st.title("Mint Token")
-
-    # Connect to Snowflake
-    my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
-    my_cur = my_cnx.cursor()
 
     # form 1 for token schema selection
     with st.form("Token Schema"):
@@ -25,13 +19,19 @@ def main():
             token_type = st.text_input('Token Type', "")
             materials = st.text_input('Materials', "")
             color = st.selectbox('Color', ["Green", "Black", "Silver", "Red", "Brown"]) 
-            mint = st.form_submit_button("Mint")
 
-        if mint:
-            # Insert the form data into Snowflake
-            query = f"INSERT INTO avatar_wearables (TOKEN_ID, TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{token_type}', '{materials}', '{color}')"
-            my_cur.execute(query)
-            my_cnx.commit()
-            
+            # Check for form 2 submission
+            if st.form_submit_button("Mint"):
+                # Connect to Snowflake within the form 2 block
+                my_cnx = snowflake.connector.connect(**st.secrets["INVENTORY_DB"])
+                my_cur = my_cnx.cursor()
+
+                # Insert the form data into Snowflake
+                query = f"INSERT INTO avatar_wearables (TOKEN_ID, TYPE, MATERIALS, COLOR) VALUES ('{token_id}','{token_type}', '{materials}', '{color}')"
+                my_cur.execute(query)
+                my_cnx.commit()
+                st.success(f"Form 2 submitted with input: {token_id, token_type, materials, color}")
+
 if __name__ == "__main__":
     main()
+
