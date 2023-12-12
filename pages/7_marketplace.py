7b70a9f339e4658e5bdc1a033ab99e9d3be0f673
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-12 16:45:18 -0500

    Update 7_marketplace.py
    100

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -7,7 +7,7 @@ token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=Img
 token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
 token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain"
 
-st.image(token_1, caption="Token: X", use_column_width=True, width=200)
-st.image(token_2, caption="Token: X", use_column_width=True, width=200)
-st.image(token_3, caption="Token: X", use_column_width=True, width=200)
+st.image(token_1, caption="Token: X", use_column_width=True, width=100)
+st.image(token_2, caption="Token: X", use_column_width=True, width=100)
+st.image(token_3, caption="Token: X", use_column_width=True, width=100)
 
