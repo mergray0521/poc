1395cfd54c9c44a70c82dd099c536d327b3b663d
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-14 15:44:01 -0500

    Update 7_marketplace.py
    try containers around each image

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -2,15 +2,6 @@ import streamlit as st
 
 st.header("Token Marketplace")
 
-container = st.container(border=True)
-container.write("This is inside the container")
-st.write("This is outside the container")
-
-# Now insert some more in the container
-container.write("This is inside too")
-
-st.write("This is outside the container")
-
 # Sample image URLs
 token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
 token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
@@ -22,25 +13,33 @@ token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=Img
 # Create three columns
 col1, col2, col3 = st.columns(3)
 
-# Display images in columns
-col1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
-col1.text("1,000 points")
-col1.button("Purchase My Say")
-col1.image(token_4, caption="Minion Glasses", use_column_width=True, width=300)
-col1.text("2,000 points")
-col1.button("Purchase Glasses")
+# Display images in containers within columns
+with col1.container():
+    col1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
+    col1.text("1,000 points")
+    col1.button("Purchase My Say")
 
-col2.image(token_2, caption="My Way Token", use_column_width=True, width=50)
-col2.text("2,000 points")
-col2.button("Purchase My Way")
-col2.image(token_5, caption="Park Pass", use_column_width=True, width=50)
-col2.text("4,000 points")
-col2.button("Purchase Pass")
+with col1.container():
+    col1.image(token_4, caption="Minion Glasses", use_column_width=True, width=300)
+    col1.text("2,000 points")
+    col1.button("Purchase Glasses")
 
-col3.image(token_3, caption="My Day Token", use_column_width=True, width=50)
-col3.text("3,000 points")
-col3.button("Purchase My Day")
-col3.image(token_6, caption="Dragon", use_column_width=True, width=50)
-col3.text("3,000 points")
-col3.button("Purchase Dragon")
+with col2.container():
+    col2.image(token_2, caption="My Way Token", use_column_width=True, width=50)
+    col2.text("2,000 points")
+    col2.button("Purchase My Way")
 
+with col2.container():
+    col2.image(token_5, caption="Park Pass", use_column_width=True, width=50)
+    col2.text("4,000 points")
+    col2.button("Purchase Pass")
+
+with col3.container():
+    col3.image(token_3, caption="My Day Token", use_column_width=True, width=50)
+    col3.text("3,000 points")
+    col3.button("Purchase My Day")
+
+with col3.container():
+    col3.image(token_6, caption="Dragon", use_column_width=True, width=50)
+    col3.text("3,000 points")
+    col3.button("Purchase Dragon")
