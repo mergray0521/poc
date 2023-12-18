c99c53e48cd5fbdf1fcb725fbce1a207a8cdf60b
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 12:09:35 -0500

    Update dataprof.py
    s

--- pages/dataprof.py
+++ pages/dataprof.py
@@ -24,7 +24,7 @@ box_height = 150
 
 # Loop over columns and apply styling
 for i in range(num_columns):
-    col = st.column()
+    col = st.columns()
     col.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column {i+1}</div>', unsafe_allow_html=True)
     col.metric(f"Metric {i+1}", f"Value {i+1}", f"Change {i+1}%")
 
