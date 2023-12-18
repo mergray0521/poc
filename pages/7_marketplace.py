f49822f7c349ea705ab0e945fb589c45ee7ec72b
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 11:53:52 -0500

    Update 7_marketplace.py
    try to combine w/ css

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -1,10 +1,19 @@
+import requests
 import streamlit as st
 
-with open('style.css') as f:
-        st.markdown(f'<style>(f.read()),<style>', unsafe_allow_html=True)
-    
+# GitHub repository details
+github_repo = "mergray0521/poc"
+css_file_path = "pages/style.css"
 
-st.header("Token Marketplace")
+# Construct the raw URL for the CSS file
+raw_url = f'https://raw.githubusercontent.com/{github_repo}/main/{css_file_path}'
+
+# Fetch the CSS content from GitHub
+response = requests.get(raw_url)
+css_content = response.text
+
+# Apply the CSS to Streamlit
+st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
 # Sample image URLs
 token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
@@ -17,30 +26,17 @@ token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=Img
 # Create three columns
 col1, col2, col3 = st.columns(3)
 
-# Display images in containers within columns
-with col1.container(border=True):
-    col1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
-    col1.text("1,000 points")
-    col1.button("Purchase My Say")
+# Loop over columns and apply styling and content
+for i, col in enumerate([col1, col2, col3]):
+    with col.container(border=True):
+        # Adjust the content inside the div according to your styling
+        col.markdown(f'<div class="css-1r6slb0 e1tzin5v2">Content for Column {i+1}</div>', unsafe_allow_html=True)
 
-    col1.image(token_4, caption="Minion Glasses", use_column_width=True, width=300)
-    col1.text("2,000 points")
-    col1.button("Purchase Glasses")
+        # Display images in containers within columns
+        col.image(token_1, caption="My Say Token", use_column_width=True, width=300)
+        col.text("1,000 points")
+        col.button("Purchase My Say")
 
-with col2.container(border=True):
-    col2.image(token_2, caption="My Way Token", use_column_width=True, width=50)
-    col2.text("2,000 points")
-    col2.button("Purchase My Way")
-
-    col2.image(token_5, caption="Park Pass", use_column_width=True, width=50)
-    col2.text("4,000 points")
-    col2.button("Purchase Pass")
-
-with col3.container(border=True):
-    col3.image(token_3, caption="My Day Token", use_column_width=True, width=50)
-    col3.text("3,000 points")
-    col3.button("Purchase My Day")
-
-    col3.image(token_6, caption="Dragon", use_column_width=True, width=50)
-    col3.text("3,000 points")
-    col3.button("Purchase Dragon")
+        col.image(token_4, caption="Minion Glasses", use_column_width=True, width=300)
+        col.text("2,000 points")
+        col.button("Purchase Glasses")
