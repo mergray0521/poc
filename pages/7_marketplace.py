7b2351534645c06ea5846a7a444b556cb5cfa624
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 10:20:41 -0500

    Update 7_marketplace.py
    add style.css up top

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+with open('style.css') as f:
+        st.markdown(f'<style>(f.read()),<style>', unsafe_allow_html=True)
+    
+
 st.header("Token Marketplace")
 
 # Sample image URLs
