1f869a3ddd74db936e84ae46cb1054e71a4f8d87
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 11:27:28 -0500

    Update dataprof.py

--- pages/dataprof.py
+++ pages/dataprof.py
@@ -1,9 +1,23 @@
+import requests
 import streamlit as st
 
-with open("mergray0521/poc/pages/style.css") as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# GitHub repository details
+github_repo = "mergray0521/poc"
+css_file_path = "pages/style.css"
 
+# Construct the raw URL for the CSS file
+raw_url = f'https://raw.githubusercontent.com/{github_repo}/main/{css_file_path}'
+
+# Fetch the CSS content from GitHub
+response = requests.get(raw_url)
+css_content = response.text
+
+# Apply the CSS to Streamlit
+st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
+
+# Rest of your Streamlit code
 col1, col2, col3 = st.columns(3)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
+
