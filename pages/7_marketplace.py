841bb210324cfdaa0a86e9d3b17de7ec4ad9136b
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 12:00:53 -0500

    Update 7_marketplace.py

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -18,9 +18,13 @@ st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 # Number of columns
 num_columns = 3
 
+# Set explicit width and height for the gray boxes
+box_width = 200
+box_height = 150
+
 # Loop over columns and apply styling
 for i in range(num_columns):
     col = st.column()
-    col.markdown(f'<div class="css-1r6slb0 e1tzin5v2">Content for Column {i+1}</div>', unsafe_allow_html=True)
+    col.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column {i+1}</div>', unsafe_allow_html=True)
     col.metric(f"Metric {i+1}", f"Value {i+1}", f"Change {i+1}%")
 
