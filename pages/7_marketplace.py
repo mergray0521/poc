7aa37edb4645d1ee31e2dfd04b51104c2d6ff925
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 11:58:45 -0500

    Update 7_marketplace.py
    try

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -16,27 +16,23 @@ css_content = response.text
 st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
 # Sample image URLs
-token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
-token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
-token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain"
-token_4 = "https://th.bing.com/th/id/OIP.OLAyoeOOoWuXs2aaZ9FL9QHaKl?rs=1&pid=ImgDetMain"
-token_5 = "https://mickeystravel.com/site/universal/files/2016/06/Universal-Orlando-base-Ticket.png"
-token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=ImgDetMain"
+image_urls = [
+    "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain",
+    "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png",
+    "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain"
+]
 
-# Create three columns
-col1, col2, col3 = st.columns(3)
+# Number of columns
+num_columns = 3
 
-# Loop over columns and apply styling and content
-for i, col in enumerate([col1, col2, col3]):
-    with col.container(border=True):
-        # Adjust the content inside the div according to your styling
-        col.markdown(f'<div class="css-1r6slb0 e1tzin5v2">Content for Column {i+1}</div>', unsafe_allow_html=True)
+# Loop over columns and apply styling
+for i in range(num_columns):
+    col = st.column()
 
-        # Display images in containers within columns
-        col.image(token_1, caption="My Say Token", use_column_width=True, width=300)
-        col.text("1,000 points")
-        col.button("Purchase My Say")
+    # Adjust the content inside the div according to your styling
+    col.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column {i+1}</div>', unsafe_allow_html=True)
 
-        col.image(token_4, caption="Minion Glasses", use_column_width=True, width=300)
-        col.text("2,000 points")
-        col.button("Purchase Glasses")
+    # Display image, description, and button within columns
+    col.image(image_urls[i], caption=f"Image {i+1}", use_column_width=True, width=300)
+    col.text(f"Description for Column {i+1}")
+    col.button(f"Button for Column {i+1}")
