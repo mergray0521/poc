9c1678a97c42db938c253bd1e0beab720727abec
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 12:14:36 -0500

    Update 7_marketplace.py
    try w image + desc

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -20,11 +20,32 @@ num_columns = 3
 
 # Set explicit width and height for the gray boxes
 box_width = 200
-box_height = 150
+box_height = 200
 
-cols = st.columns(3)  # Adjust the number as needed
-col = cols[0]  # Use the desired column
+cols = st.columns(num_columns)
+
+# Sample data (replace with your actual data)
+data = [
+    {"image_url": "https://example.com/image1.jpg", "description": "Description 1"},
+    {"image_url": "https://example.com/image2.jpg", "description": "Description 2"},
+    {"image_url": "https://example.com/image3.jpg", "description": "Description 3"},
+]
 
 # Loop over columns and apply styling
 for i, col in enumerate(cols):
-    col.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column {i+1}</div>', unsafe_allow_html=True)
+    # Access data for the current column
+    current_data = data[i]
+
+    # Display image
+    col.image(current_data["image_url"], width=box_width, caption=f"Image {i+1}")
+
+    # Display description
+    col.write(current_data["description"])
+
+    # Display button
+    if col.button(f"Button {i+1}"):
+        # Action to perform when the button is clicked
+        st.write(f"Button {i+1} clicked!")
+
+# You can customize the content of each box based on your specific requirements.
+
