a042871b390f906de8755a989a042926a1c92fd8
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 11:39:37 -0500

    Update dataprof.py

--- pages/dataprof.py
+++ pages/dataprof.py
@@ -16,17 +16,15 @@ css_content = response.text
 st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
 # Individual styling for each column
-col1 = st.columns()
+col1, col2, col3 = st.columns(3)
+
 col1.markdown('<div class="css-1r6slb0 e1tzin5v2">Styled content for Temperature</div>', unsafe_allow_html=True)
 col1.metric("Temperature", "70 °F", "1.2 °F")
 
-col2 = st.columns()
 col2.markdown('<div class="css-1r6slb0 e1tzin5v2">Styled content for Wind</div>', unsafe_allow_html=True)
 col2.metric("Wind", "9 mph", "-8%")
 
-col3 = st.columns()
 col3.markdown('<div class="css-1r6slb0 e1tzin5v2">Styled content for Humidity</div>', unsafe_allow_html=True)
 col3.metric("Humidity", "86%", "4%")
-
 
 
