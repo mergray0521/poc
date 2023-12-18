a11bed8ce4bd155de8b3bdf27690a0d2b71b1afe
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 12:30:55 -0500

    Update dataprof.py
    back

--- pages/dataprof.py
+++ pages/dataprof.py
@@ -15,26 +15,19 @@ css_content = response.text
 # Apply the CSS to Streamlit
 st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
+# Number of columns
+num_columns = 3
+
 # Set explicit width and height for the gray boxes
 box_width = 200
-box_height = 200
+box_height = 150
 
-# Column 1
-col1 = st.columns(1)[0]
+cols = st.columns(3)  # Adjust the number as needed
+col = cols[0]  # Use the desired column
 
-# Customize content for column 1
-col1.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column 1</div>', unsafe_allow_html=True)
-
-# Column 2
-col2 = st.columns(1)[0]
-
-# Customize content for column 2
-col2.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column 2</div>', unsafe_allow_html=True)
-
-# Column 3
-col3 = st.columns(1)[0]
-
-# Customize content for column 3
-col3.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column 3</div>', unsafe_allow_html=True)
+# Loop over columns and apply styling
+for i, col in enumerate(cols):
+    col.markdown(f'<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">Content for Column {i+1}</div>', unsafe_allow_html=True)
+    col.metric(f"Metric {i+1}", f"Value {i+1}", f"Change {i+1}%")
 
 
