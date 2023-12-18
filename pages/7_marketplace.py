4f5a7d63c4b68cfb7abeeb75f79bc32d196fa86c
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 12:53:49 -0500

    Update 7_marketplace.py

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -22,42 +22,70 @@ box_height = 200
 # Sample data (replace with your actual data)
 data = [
     {
-        "image_url": "https://example.com/image1.jpg",
+        "image_url": "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain",
         "button_label": "Button 1",
         "token_name": "My Way Token",
         "token_cost": "1,000 points",
     },
     {
-        "image_url": "https://example.com/image2.jpg",
+        "image_url": "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png",
         "button_label": "Button 2",
         "token_name": "My Day Token",
         "token_cost": "2,000 points",
     },
     {
-        "image_url": "https://example.com/image3.jpg",
+        "image_url": "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain",
         "button_label": "Button 3",
         "token_name": "My Say Token",
         "token_cost": "3,000 points",
     },
 ]
 
-# Create a row with multiple columns
-cols = st.beta_columns(len(data), wrap_in_container=False)
+# Column 1
+col1 = st.columns(1)[0]
 
-# Loop over data and apply styling
-for i, col in enumerate(cols):
-    # Access data for the current column
-    current_data = data[i]
-
-    # Customize content for the column
-    content = f'''
-    <div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">
-        <img src="{current_data["image_url"]}" style="max-width:100%; height:auto;" />
-        <div class="caption">
-            <p>{current_data["token_name"]}</p>
-            <p>{current_data["token_cost"]}</p>
-        </div>
-        <button class="button">{current_data["button_label"]}</button>
+# Customize content for column 1
+content1 = f'''
+<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">
+    <img src="{data[0]["image_url"]}" style="max-width:100%; height:auto;" />
+    <div class="caption">
+        <p>{data[0]["token_name"]}</p>
+        <p>{data[0]["token_cost"]}</p>
     </div>
-    '''
-    col.markdown(content, unsafe_allow_html=True)
+    <button>{data[0]["button_label"]}</button>
+</div>
+'''
+col1.markdown(content1, unsafe_allow_html=True)
+
+# Column 2
+col2 = st.columns(1)[0]
+
+# Customize content for column 2
+content2 = f'''
+<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">
+    <img src="{data[1]["image_url"]}" style="max-width:100%; height:auto;" />
+    <div class="caption">
+        <p>{data[1]["token_name"]}</p>
+        <p>{data[1]["token_cost"]}</p>
+    </div>
+    <button>{data[1]["button_label"]}</button>
+</div>
+'''
+col2.markdown(content2, unsafe_allow_html=True)
+
+# Column 3
+col3 = st.columns(1)[0]
+
+# Customize content for column 3
+content3 = f'''
+<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">
+    <img src="{data[2]["image_url"]}" style="max-width:100%; height:auto;" />
+    <div class="caption">
+        <p>{data[2]["token_name"]}</p>
+        <p>{data[2]["token_cost"]}</p>
+    </div>
+    <button>{data[2]["button_label"]}</button>
+</div>
+'''
+col3.markdown(content3, unsafe_allow_html=True)
+     
