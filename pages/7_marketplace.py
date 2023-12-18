65ea3eb8a93bfa43f7e6e0bc20fa489fa1f54512
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 12:35:16 -0500

    Update 7_marketplace.py

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -15,40 +15,51 @@ css_content = response.text
 # Apply the CSS to Streamlit
 st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
-# Number of columns
-num_columns = 3
-
 # Set explicit width and height for the gray boxes
 box_width = 200
 box_height = 200
 
-cols = st.columns(num_columns)
-
+# Sample data (replace with your actual data)
 data = [
-    {"image_url": "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain", "description": "Token 1"},
-    {"image_url": "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.pn", "description": "Token 2"},
-    {"image_url": "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain", "description": "Token 3"},
+    {"image_url": "https://example.com/image1.jpg", "button_label": "Button 1"},
+    {"image_url": "https://example.com/image2.jpg", "button_label": "Button 2"},
+    {"image_url": "https://example.com/image3.jpg", "button_label": "Button 3"},
 ]
 
-# Loop over columns and apply styling
-for i, col in enumerate(cols):
-    # Access data for the current column
-    current_data = data[i]
+# Column 1
+col1 = st.columns(1)[0]
 
-    # Apply existing markdown styling
-    col.markdown(f'<div class="css-1r6slb0.e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">', unsafe_allow_html=True)
+# Customize content for column 1
+content1 = f'''
+<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">
+    <img src="{data[0]["image_url"]}" style="max-width:100%; height:auto;" />
+    <button>{data[0]["button_label"]}</button>
+</div>
+'''
+col1.markdown(content1, unsafe_allow_html=True)
 
-    # Display image
-    col.image(current_data["image_url"], width=box_width, caption=f"Image {i+1}")
+# Column 2
+col2 = st.columns(1)[0]
 
-    # Display description
-    col.markdown(f'<div class="description">{current_data["description"]}</div>', unsafe_allow_html=True)
+# Customize content for column 2
+content2 = f'''
+<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">
+    <img src="{data[1]["image_url"]}" style="max-width:100%; height:auto;" />
+    <button>{data[1]["button_label"]}</button>
+</div>
+'''
+col2.markdown(content2, unsafe_allow_html=True)
 
-    # Display button
-    if col.button(f"Button {i+1}"):
-        # Action to perform when the button is clicked
-        st.write(f"Button {i+1} clicked!")
+# Column 3
+col3 = st.columns(1)[0]
 
-    # Close the div
-    col.markdown('</div>', unsafe_allow_html=True)
+# Customize content for column 3
+content3 = f'''
+<div class="css-1r6slb0 e1tzin5v2" style="width:{box_width}px; height:{box_height}px;">
+    <img src="{data[2]["image_url"]}" style="max-width:100%; height:auto;" />
+    <button>{data[2]["button_label"]}</button>
+</div>
+'''
+col3.markdown(content3, unsafe_allow_html=True)
+
 
