872b56b3708b99143db0dcf373da9f8df72687c6
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-18 11:37:32 -0500

    Update dataprof.py

--- pages/dataprof.py
+++ pages/dataprof.py
@@ -15,11 +15,17 @@ css_content = response.text
 # Apply the CSS to Streamlit
 st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
-# Rest of your Streamlit code
-st.markdown('<div class="css-1r6slb0 e1tzin5v2">Styled content</div>', unsafe_allow_html=True)
-col1, col2, col3 = st.columns(3)
+# Individual styling for each column
+col1 = st.column()
+col1.markdown('<div class="css-1r6slb0 e1tzin5v2">Styled content for Temperature</div>', unsafe_allow_html=True)
 col1.metric("Temperature", "70 °F", "1.2 °F")
+
+col2 = st.column()
+col2.markdown('<div class="css-1r6slb0 e1tzin5v2">Styled content for Wind</div>', unsafe_allow_html=True)
 col2.metric("Wind", "9 mph", "-8%")
+
+col3 = st.column()
+col3.markdown('<div class="css-1r6slb0 e1tzin5v2">Styled content for Humidity</div>', unsafe_allow_html=True)
 col3.metric("Humidity", "86%", "4%")
 
 
