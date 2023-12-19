5310985eef978a817723f4572c4bad5d45edd823
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-19 11:16:10 -0500

    Update 7_marketplace.py

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -62,15 +62,15 @@ with st.container():
                 </div>
 
                 <div class="custom-container">
-                    <img src="https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png" alt="My Say Token" class="custom-image">
-                    <p>1,000 points</p>
-                    <button class="custom-button">Purchase My Say</button>
+                    <img src="https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png" alt="My Way Token" class="custom-image">
+                    <p>2,000 points</p>
+                    <button class="custom-button">Purchase My Way</button>
                 </div>
 
                 <div class="custom-container">
-                    <img src="https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain" alt="My Say Token" class="custom-image">
-                    <p>1,000 points</p>
-                    <button class="custom-button">Purchase My Say</button>
+                    <img src="https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain" alt="My Day Token" class="custom-image">
+                    <p>3,000 points</p>
+                    <button class="custom-button">Purchase My Day</button>
                 </div>
 
                 
