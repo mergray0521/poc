76b430379f72fb0d8c6f3978d818f3d601bf56fa
alanna-demuro <85509060+alanna-demuro@users.noreply.github.com>
2023-12-19 16:10:25 -0700

    Create earned_points_ad.py

---
+++ pages/earned_points_ad.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**st.secrets["TOKEN_OWNERSHIP"])
+my_cur = my_cnx.cursor()
+
+st.header("Earned Points") 
+st.text("You’ve earned 50 Park points for your latest park visit! Redeem your points now for the Park Pro Token!")
+
+image_url = "https://i.pinimg.com/originals/4f/72/64/4f7264966212744a1302d8c960abe398.jpg"
+st.image(image_url, caption="+50 points", use_column_width=True)
+
+if st.button("Collect"):
+    # Insert the form data into Snowflake
+    query = f"INSERT INTO point_ownership (USER_ID, POINT_QUANTITY) VALUES ('{3}','{50}')"
+    my_cur.execute(query)
+    my_cnx.commit()
+    st.success(f"50 points added to user {3}'s wallet")
+    st.text("Proceed to marketplace page to redeem") 
