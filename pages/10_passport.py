ab4ff60fb60f7d75c5270174103f9079e161fba8
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-19 15:11:26 -0500

    Update 10_passport.py

--- pages/10_passport.py
+++ pages/10_passport.py
@@ -4,14 +4,43 @@ import snowflake.connector
 
 st.title("My Stuff")
 
+css_code = """
+    <style>
+        .container {
+            border: 2px solid #1f618d;
+            padding: 10px;
+            border-radius: 5px;
+            text-align: center;
+            margin-bottom: 10px;
+            height: 350px;
+        }
+    </style>
+"""
+
 with st.container():
   st.text('Reedemables')
-    
 
-c1, c2 = st.columns(2)
-c3, c4 = st.columns(2) #just to highlight these are different cols
+with st.container():
+  c1, c2 = st.columns(2)
+  c3, c4 = st.columns(2) #just to highlight these are different cols
+for c in [c1, c2, c3, c4]:
+  with c:
+    st.markdown(css_code, unsafe_allow_html=True)
 
-c1.write('Points')
-c2.write('Badges')
-c3.write('Park Tickets')
-c4.write('Hotel Keys')
+  
+          html_code = """
+              <div class="container">
+                    <p>Points</p>
+                </div>
+
+                <div class="container">
+                    <p>Badges</p>
+                </div>
+
+                 <div class="Park Tickets">
+                    <p>Badges</p>
+                </div>
+
+                 <div class="Hotel Keys">
+                    <p>Badges</p>
+                </div>
