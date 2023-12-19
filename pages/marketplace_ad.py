3e1fc70afa2f3f205fec330ca3d32ebb428d6355
alanna-demuro <85509060+alanna-demuro@users.noreply.github.com>
2023-12-19 08:58:40 -0700

    Update marketplace_ad.py

--- pages/marketplace_ad.py
+++ pages/marketplace_ad.py
@@ -21,6 +21,9 @@ st.header("Token Marketplace")
 
 # Sample image URLs
 token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
+token_1_text = st.text('1,00 points')
+token_1_button = st.button("Purchase My Say", key="purchase_my_say", help="my-button")
+token_1_img = st.image(token_1,  caption="My Say Token", use_column_width=True, width=300)
 token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
 token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain"
 token_4 = "https://th.bing.com/th/id/OIP.OLAyoeOOoWuXs2aaZ9FL9QHaKl?rs=1&pid=ImgDetMain"
@@ -32,13 +35,13 @@ token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=Img
 c1, c2, c3 = st.columns(3)
 c4, c5, c6 = st.columns(3) #just to highlight these are different cols
 with st.container():   
-    c1Img = c1.image(token_1,  caption="My Say Token", use_column_width=True, width=300)
-    # st.markdown('<div class="css-1r6slb0 e1tzin5v2">', unsafe_allow_html=True)
-    c1.markdown(f'<div style="color:#33ff33;background-color: #EEEEEE;border: 2px solid #CCCCCC;padding: 5% 5% 5% 10%;border-radius: 5px;height: 100px"> {c1Img} </h1>', unsafe_allow_html=True)
+    # c1Img = c1.image(token_1,  caption="My Say Token", use_column_width=True, width=300)
+    # # st.markdown('<div class="css-1r6slb0 e1tzin5v2">', unsafe_allow_html=True)
+    c1.markdown(f'<div style="color:#33ff33;background-color: #EEEEEE;border: 2px solid #CCCCCC;padding: 5% 5% 5% 10%;border-radius: 5px;height: 100px"> {token_1_button} </div>', unsafe_allow_html=True)
     # Column/Token 1
     # c1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
-    c1.text("1,000 points")
-    c1.button("Purchase My Say", key="purchase_my_say", help="my-button")
+    # c1.text("1,000 points")
+    # c1.button("Purchase My Say", key="purchase_my_say", help="my-button")
 
     # Column/Token 2
     c2.image(token_2, caption="My Way Token", use_column_width=True, width=50)
