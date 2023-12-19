1a4fca015fef1bd2c702aacf0cf0af2437f311a3
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-19 11:14:54 -0500

    Update 7_marketplace.py

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -1,38 +1,32 @@
 import requests
-import streamlit as st 
+import streamlit as st
 
-# GitHub repository details
-github_repo = "mergray0521/poc"
-css_file_path = "pages/style.css"
+# # GitHub repository details
+# github_repo = "mergray0521/poc"
+# css_file_path = "pages/style.css"
 
-# Construct the raw URL for the CSS file
-raw_url = f'https://raw.githubusercontent.com/{github_repo}/main/{css_file_path}'
+# # Construct the raw URL for the CSS file
+# raw_url = f'https://raw.githubusercontent.com/{github_repo}/main/{css_file_path}'
 
-# Fetch the CSS content from GitHub
-response = requests.get(raw_url)
-css_content = response.text
+# # Fetch the CSS content from GitHub
+# response = requests.get(raw_url)
+# css_content = response.text
 
-# Apply the CSS to Streamlit
-st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
+# # Apply the CSS to Streamlit
+# st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
 st.header("Token Marketplace")
 
-# Sample image URLs
-token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
-token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
-token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain"
-token_4 = "https://th.bing.com/th/id/OIP.OLAyoeOOoWuXs2aaZ9FL9QHaKl?rs=1&pid=ImgDetMain"
-token_5 = "https://mickeystravel.com/site/universal/files/2016/06/Universal-Orlando-base-Ticket.png"
-token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=ImgDetMain"
+# # Sample image URLs
+# token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
+# token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
+# token_3 = "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain"
+# token_4 = "https://th.bing.com/th/id/OIP.OLAyoeOOoWuXs2aaZ9FL9QHaKl?rs=1&pid=ImgDetMain"
+# token_5 = "https://mickeystravel.com/site/universal/files/2016/06/Universal-Orlando-base-Ticket.png"
+# token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=ImgDetMain"
 
-import streamlit as st
 
-with st.container():
-    col1, col2, col3 = st.columns(3)
-
-    for col in [col1, col2, col3]:
-        with col:
-            css_code = """
+css_code = """
     <style>
         .custom-container {
             border: 2px solid #1f618d;
@@ -52,7 +46,12 @@ with st.container():
         }
     </style>
 """
-            
+
+with st.container():
+    col1, col2, col3 = st.columns(3)
+
+    for col in [col1, col2, col3]:
+        with col:
             st.markdown(css_code, unsafe_allow_html=True)
 
             html_code = """
@@ -74,6 +73,7 @@ with st.container():
                     <button class="custom-button">Purchase My Say</button>
                 </div>
 
+                
             """
             st.markdown(html_code, unsafe_allow_html=True)
 
