e68fa85fab5962d6468fe1815c46bbc52d9057b5
alanna-demuro <85509060+alanna-demuro@users.noreply.github.com>
2023-12-19 08:33:00 -0700

    Update marketplace_ad.py

--- pages/marketplace_ad.py
+++ pages/marketplace_ad.py
@@ -39,9 +39,10 @@ with st.container(border=True):
 
     # Column/Token 2
     c2.image(token_2, caption="My Way Token", use_column_width=True, width=50)
-    c2.text("2,000 points")
+    c2.markdown("2,000 points")
+    # c2.text("2,000 points")
     c2.button("Purchase My Way", key="purchase_my_way", help="my-button")
-    c2.markdown(f'<h1 style="color:#33ff33;background-color: #EEEEEE;border: 2px solid #CCCCCC;padding: 5% 5% 5% 10%;border-radius: 5px;height: 100px">{"ColorMeBlue text”"}</h1>', unsafe_allow_html=True)
+    # c2.markdown(f'<h1 style="color:#33ff33;background-color: #EEEEEE;border: 2px solid #CCCCCC;padding: 5% 5% 5% 10%;border-radius: 5px;height: 100px">{"ColorMeBlue text”"}</h1>', unsafe_allow_html=True)
     # Column/Token 3
     c3.image(token_3, caption="My Day Token", use_column_width=True, width=50)
     c3.text("3,000 points")
