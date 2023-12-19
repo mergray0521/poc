e0fa5c2901942fde43fe23c7be217fb640c3b341
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-19 11:26:54 -0500

    Update 7_marketplace.py

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -1,9 +1,6 @@
-import requests
 import streamlit as st
 
 st.header("Token Marketplace")
-
-import streamlit as st
 
 css_code = """
     <style>
@@ -27,7 +24,7 @@ css_code = """
 """
 
 with st.container():
-     tokens = [
+    tokens = [
         {"image_url": "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain", "name": "My Say Token", "points": "1,000"},
         {"image_url": "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png", "name": "My Way Token", "points": "2,000"},
         {"image_url": "https://th.bing.com/th/id/OIP.T2FQy8uhLgynn5M-UTI0ZAHaHa?rs=1&pid=ImgDetMain", "name": "My Day Token", "points": "3,000"},
@@ -36,24 +33,26 @@ with st.container():
         {"image_url": "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=ImgDetMain", "name": "My Day Token", "points": "3,000"},
     ]
 
-        tokens_per_row = 3
+    tokens_per_row = 3
 
-        for i in range(0, len(tokens), tokens_per_row):
-            row_tokens = tokens[i:i + tokens_per_row]
-    
-            col1, col2, col3 = st.columns(3)
-    
-            for col, token in zip([col1, col2, col3], row_tokens):
-                with col:
-                    st.markdown(css_code, unsafe_allow_html=True)
-    
-                    html_code = f"""
-                        <div class="custom-container">
-                            <img src="{token['image_url']}" alt="{token['name']}" class="custom-image">
-                            <p>{token['points']} points</p>
-                            <button class="custom-button">Purchase {token['name']}</button>
-                        </div>
-                    """
+    for i in range(0, len(tokens), tokens_per_row):
+        row_tokens = tokens[i:i + tokens_per_row]
+
+        col1, col2, col3 = st.columns(3)
+
+        for col, token in zip([col1, col2, col3], row_tokens):
+            with col:
+                st.markdown(css_code, unsafe_allow_html=True)
+
+                html_code = f"""
+                    <div class="custom-container">
+                        <img src="{token['image_url']}" alt="{token['name']}" class="custom-image">
+                        <p>{token['points']} points</p>
+                        <button class="custom-button">Purchase {token['name']}</button>
+                    </div>
+                """
+                st.markdown(html_code, unsafe_allow_html=True)
+
                     st.markdown(html_code, unsafe_allow_html=True)
     
     
