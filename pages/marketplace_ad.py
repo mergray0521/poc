29a0ab5d06f798e8287d9fe885bde4b86d677f15
alanna-demuro <85509060+alanna-demuro@users.noreply.github.com>
2023-12-19 08:15:11 -0700

    Update marketplace_ad.py

--- pages/marketplace_ad.py
+++ pages/marketplace_ad.py
@@ -17,6 +17,8 @@ st.markdown(f'<style>{css_content}</style>', unsafe_allow_html=True)
 
 st.header("Token Marketplace")
 
+
+
 # Sample image URLs
 token_1 = "https://th.bing.com/th/id/OIP.Xk44653VMX5ZhgDi0h1oIQHaE8?rs=1&pid=ImgDetMain"
 token_2 = "https://cdn4.iconfinder.com/data/icons/slot-machine-icons/200/casino_token-512.png"
@@ -29,26 +31,44 @@ token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=Img
 # col1, col2, col3 = st.columns(3)
 c1, c2, c3 = st.columns(3)
 c4, c5, c6 = st.columns(3) #just to highlight these are different cols
-with st.container():     
+with st.container(border=True):   
+    # Column/Token 1
     c1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
     c1.text("1,000 points")
     c1.button("Purchase My Say", key="purchase_my_say", help="my-button")
 
-    c2.write("c2")
-    c2.image(token_4, caption="Minion Glasses", use_column_width=True, width=300)
+    # Column/Token 2
+    c2.image(token_2, caption="My Way Token", use_column_width=True, width=50)
     c2.text("2,000 points")
-    c2.button("Purchase Glasses", key="purchase_glasses", help="my-button")
-    c3.write("c3") 
-with st.container():     
-    c4.write("c4")     
-    c5.write("c5")     
-    c6.write("c6")
+    c2.button("Purchase My Way", key="purchase_my_way", help="my-button")
+
+    # Column/Token 3
+    c3.image(token_3, caption="My Day Token", use_column_width=True, width=50)
+    c3.text("3,000 points")
+    c3.button("Purchase My Day", key="purchase_my_day", help="my-button") 
+
+#second row
+with st.container(border=True):     
+    # Column/Token 4
+    c4.image(token_4, caption="Minion Glasses", use_column_width=True, width=300)
+    c4.text("2,000 points")
+    c4.button("Purchase Glasses", key="purchase_glasses", help="my-button")
+    
+    # Column/Token 5
+    c5.image(token_5, caption="Park Pass", use_column_width=True, width=50)
+    c5.text("4,000 points")
+    c5.button("Purchase Pass", key="purchase_pass", help="my-button")  
+
+    # Column/Token 6
+    c6.image(token_6, caption="Dragon", use_column_width=True, width=50)
+    c6.text("3,000 points")
+    c6.button("Purchase Dragon", key="purchase_dragon", help="my-button")
 
 
 
 # Display images in containers within columns
 # with col1:
-#     with st.container():
+#     with st.container(border=True):
 #         st.markdown('<div class="css-1r6slb0 e1tzin5v2">', unsafe_allow_html=True)
 #         st.image(token_1, caption="My Say Token", use_column_width=True, width=300)
 #         st.text("1,000 points")
