c6a20142611a763bc89208a63ff6e8dcb951612d
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-19 14:14:47 -0500

    Update 7_marketplace.py
    3

--- pages/7_marketplace.py
+++ pages/7_marketplace.py
@@ -11,7 +11,7 @@ css_code = """
             border-radius: 5px;
             text-align: center;
             margin-bottom: 10px;
-            height: 200px;
+            height: 300px;
         }
 
         .custom-image {
