77d060fdb2cbe6bff190587ace114c6f66ae0dd1
alanna-demuro <85509060+alanna-demuro@users.noreply.github.com>
2023-12-19 08:40:27 -0700

    Update marketplace_ad.py

--- pages/marketplace_ad.py
+++ pages/marketplace_ad.py
@@ -32,7 +32,7 @@ token_6 = "https://th.bing.com/th/id/OIP.2JqerB2uBGAwciMukwF5ygHaHJ?rs=1&pid=Img
 c1, c2, c3 = st.columns(3)
 c4, c5, c6 = st.columns(3) #just to highlight these are different cols
 with st.container():   
-    st.markdown('<div class="css-1r6slb0 e1tzin5v2">', unsafe_allow_html=True)
+    # st.markdown('<div class="css-1r6slb0 e1tzin5v2">', unsafe_allow_html=True)
     # Column/Token 1
     c1.image(token_1, caption="My Say Token", use_column_width=True, width=300)
     c1.text("1,000 points")
