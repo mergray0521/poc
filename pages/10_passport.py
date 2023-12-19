a2c65bb98be2f4c77495464362a973dde0beae26
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-19 14:59:26 -0500

    Update 10_passport.py

--- pages/10_passport.py
+++ pages/10_passport.py
@@ -2,5 +2,11 @@ import streamlit as st
 import pandas as pd
 import snowflake.connector
 
-st.title("Meredith's Passport")
+st.title("My Stuff")
 
+  with st.container():
+    st.text('Reedemables')
+    
+    col1, col2 = st.columns(2)
+    col1.write('Points')
+    col2.write('Badges')
