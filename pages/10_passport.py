49ffa6db17e1dd1565118c29d8475e02ba86131c
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-20 18:12:01 -0500

    Update 10_passport.py

--- pages/10_passport.py
+++ pages/10_passport.py
@@ -73,11 +73,13 @@ cols_row2[1].markdown(html_code_row2_right, unsafe_allow_html=True)
 cols_row3 = st.columns(2)
 html_code_row3_left = """
     <div class="custom-box">
+    <img src= "https://th.bing.com/th/id/OIP.uf88vISNWrzqmU40p2Ya4AAAAA?rs=1&pid=ImgDetMain class="smaller-image">
     <h3 class="custom-header">Park Tickets</h3>
     </div>
 """
 html_code_row3_right = """
     <div class="custom-box">
+     <img src= "https://th.bing.com/th/id/R.02023a7862e44b743993fcd7b6095ed2?rik=fcRJUsL91OVAQA&riu=http%3a%2f%2ficons.iconarchive.com%2ficons%2fpaomedia%2fsmall-n-flat%2f1024%2fkey-icon.png&ehk=wJ2fX%2bHE%2f0Xi7FtOUI7tSvW4Z0%2b7m0lonNU61mtdwOM%3d&risl=&pid=ImgRaw&r=0" class="smaller-image">
     <h3 class="custom-header">Hotel Keys</h3>
     </div>
 """
