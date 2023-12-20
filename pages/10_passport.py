699f8c2e406c96fe793aedcb81571bbb41c6f271
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-20 14:57:35 -0500

    Update 10_passport.py

--- pages/10_passport.py
+++ pages/10_passport.py
@@ -17,7 +17,7 @@ css_code = """
         .custom-image {
             width: 100%;
             border-radius: 5px;
-            height: 150px;
+            height: 300px;
         }
     </style>  
 """
