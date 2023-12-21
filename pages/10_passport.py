678a47fe6f674f7150286929b1f0dab216de0813
mergray0521 <150840293+mergray0521@users.noreply.github.com>
2023-12-21 11:51:27 -0500

    Update 10_passport.py

--- pages/10_passport.py
+++ pages/10_passport.py
@@ -20,6 +20,9 @@ if result:
    points_df = pd.DataFrame(result, columns=["User_ID", "Point_Quantity"])
    st.dataframe(points_df)
 
+# Assuming points_df is already defined
+point_quantity_value = points_df["Point_Quantity"].iloc[0] if not points_df.empty else "N/A"
+
 
 st.title("My Stuff")
 
@@ -106,7 +109,7 @@ html_code_row2_left = """
     <div class="custom-box">
     <h3 class="custom-header">Points</h3>
     <img src= "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(15).png?raw=true class="smaller-image">
-    <p>{points_df}</p>
+    <p>{point_quantity_value}</p>
     </div>
 """
 html_code_row2_right = """
