e268d3dbeddf542e719144606ae66da6138bac11
mergray0521 <mergray0521@gmail.com>
2023-12-22 12:19:55 -0500

    location dates

--- pages/10_passport.py
+++ pages/10_passport.py
@@ -46,8 +46,8 @@ park_ticket_info = []
 
 for info in park_ticket_result:
     ticket_info = (
-        f" {info[1]}, {info[2]}\n"
-        f {info[3].strftime('%m/%d/%y')} - {info[4].strftime('%m/%d/%y')}"
+        f"Location: {info[1]}, {info[2]}\n"
+        f"Dates: {info[3].strftime('%m/%d/%y')} - {info[4].strftime('%m/%d/%y')}"
     )
     park_ticket_info.append(ticket_info)
 
