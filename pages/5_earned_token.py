612b5b06e4abc1b59cb3697c75469c41df408aa0
mergray0521 <mergray0521@gmail.com>
2023-12-28 13:06:41 -0500

    my way

--- pages/5_earned_token.py
+++ pages/5_earned_token.py
@@ -6,17 +6,17 @@ my_cnx = snowflake.connector.connect(**st.secrets["TOKEN_OWNERSHIP"])
 my_cur = my_cnx.cursor()
 
 st.header("Earned New Token") 
-st.text("Congrats on recruiting your team of Minions! Here is your Minion Mayhem Badge!")
+st.text("Congrats on collecting a My Way Token! This influence token allows you to vote on Universal polls + topics.")
 
-image_url = "https://i.ytimg.com/vi/H-7_dOS5z9s/maxresdefault.jpg"
-st.image(image_url, caption="Token: Minion Mayhem", use_column_width=True)
+image_url = "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(16).png?raw=true"
+st.image(image_url, caption="Token: My Way", use_column_width=True)
 
 if st.button("Collect"):
     # Insert the form data into Snowflake
     query = f"INSERT INTO token_ownership (TOKEN_ID, OWNER_ID, QUANTITY) VALUES ('{605}','{3}', '{1}')"
     my_cur.execute(query)
     my_cnx.commit()
-    st.success(f"Minion Mayhem token added to user {3}'s wallet")
+    st.success(f"My Way token added to user {1}'s wallet")
 
 
 
