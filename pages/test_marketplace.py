0ce3b55be1800c104ae0bbf5dfbc4fc665a24d4a
mergray0521 <mergray0521@gmail.com>
2023-12-28 11:08:27 -0500

    token cost and id

--- pages/test_marketplace.py
+++ pages/test_marketplace.py
@@ -33,15 +33,16 @@ css_code = """
 
 # Token details
 tokens = [
-    {"name": "My Say Token", "token_id" : "1201", "cost": 1000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(16).png?raw=true"},
-    {"name": "My Way Token", "token_id" : "1301", "cost": 2000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(17).png?raw=true"},
-    {"name": "My Day Token", "token_id" : "1402", "cost": 3000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(15).png?raw=true"},
-    {"name": "Park Ticket", "token_id" : "1002", "cost": 4000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/ticket.png?raw=true"},
-    {"name": "Dragon", "token_id" : "401", "cost": 4000, "image_url": "https://cdn.dribbble.com/users/1061278/screenshots/14605165/media/f27c0bfd48d70f3aa755d3617b287f3e.png?resize=400x300&vertical=center"},
-    {"name": "Hatching Egg", "token_id" : "108", "cost": 3000, "image_url": "https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Dragon_Egg-512.png"},
+    {"name": "my_say", "token_id" : "1201", "token_cost": 1000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(16).png?raw=true"},
+    {"name": "my_way", "token_id" : "1301", "token_cost": 2000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(17).png?raw=true"},
+    {"name": "my_day", "token_id" : "1402", "token_cost": 3000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(15).png?raw=true"},
+    {"name": "park_ticket", "token_id" : "1002", "token_cost": 4000, "image_url": "https://github.com/mergray0521/poc/blob/main/images/ticket.png?raw=true"},
+    {"name": "trained_dragon", "token_id" : "401", "token_cost": 4000, "image_url": "https://cdn.dribbble.com/users/1061278/screenshots/14605165/media/f27c0bfd48d70f3aa755d3617b287f3e.png?resize=400x300&vertical=center"},
+    {"name": "dragon_egg", "token_id" : "108", "token_cost": 3000, "image_url": "https://cdn3.iconfinder.com/data/icons/fantasy-and-role-play-game-adventure-quest/512/Dragon_Egg-512.png"},
 ]
 
 user_id = 1
+
 
 # Function to handle button click
 def handle_purchase(token_name, token_cost):
