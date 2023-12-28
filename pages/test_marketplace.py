9a313edf0e1a28a43ae84208deb3869dddff83b2
mergray0521 <mergray0521@gmail.com>
2023-12-28 09:39:15 -0500

    try no img

--- pages/test_marketplace.py
+++ pages/test_marketplace.py
@@ -1,31 +1,6 @@
-import requests
 import streamlit as st
 import snowflake.connector
 
-st.header("Token Marketplace")
-css_code = """
-    <style>
-        .custom-container {
-            border: 2px solid #1f618d;
-            padding: 10px;
-            border-radius: 5px;
-            text-align: center;
-            margin-bottom: 10px;
-            height: 280px;
-        }
-
-        .custom-image {
-            width: 80%;
-            border-radius: 5px;
-            height: 150px;
-        }
-
-        .custom-button {
-            margin-top: 10px;
-            border: 2px solid #429DF5;
-        }
-    </style>
-"""
 
 # Token cost dictionary
 token_costs = {
@@ -37,7 +12,7 @@ token_costs = {
     "Hatching Egg": 3000,
 }
 
-
+# Assuming you have the user_id (replace with your actual user_id)
 user_id = 1
 
 # Function to handle button click
@@ -63,6 +38,31 @@ def handle_purchase(token_name, token_cost):
     else:
         st.error("Insufficient points to purchase this token.")
 
+# UI Code
+st.header("Token Marketplace")
+css_code = """
+    <style>
+        .custom-container {
+            border: 2px solid #1f618d;
+            padding: 10px;
+            border-radius: 5px;
+            text-align: center;
+            margin-bottom: 10px;
+            height: 280px;
+        }
+
+        .custom-image {
+            width: 80%;
+            border-radius: 5px;
+            height: 150px;
+        }
+
+        .custom-button {
+            margin-top: 10px;
+            border: 2px solid #429DF5;
+        }
+    </style>
+"""
 
 # Assuming you have defined st.container somewhere in your code
 with st.container():
@@ -70,11 +70,6 @@ with st.container():
     for col, token_name in zip([c1, c2, c3], ["My Say Token", "My Way Token", "My Day Token"]):
         with col:
             col.markdown(css_code, unsafe_allow_html=True)
-            html_code_token = f"""
-                <div class="custom-container">
-                    <img src="https://github.com/mergray0521/poc/blob/main/images/MicrosoftTeams-image%20(16).png?raw=true" alt="{token_name}" class="custom-image">
-                    <p>{token_costs[token_name]} points</p>
-                    <button class="custom-button" onclick="handle_purchase('{token_name}', {token_costs[token_name]})">Purchase {token_name}</button>
-                </div>
-            """
-            col.markdown(html_code_token, unsafe_allow_html=True)
+            # Use st.button to handle button clicks
+            if st.button(f"Purchase {token_name} - {token_costs[token_name]} points"):
+                handle_purchase(token_name, token_costs[token_name])
