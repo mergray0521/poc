3f4d37b02f48a31c4a91d4a9f610285561d33229
mergray0521 <mergray0521@gmail.com>
2023-12-28 11:29:32 -0500

    back

--- pages/test_marketplace.py
+++ pages/test_marketplace.py
@@ -71,11 +71,11 @@ with st.container():
     for col, token in zip([c1, c2, c3, c4, c5, c6], tokens):
         with col:
             col.markdown(css_code, unsafe_allow_html=True)
-            col.markdown(f'<div class="custom-container"><img src="{token["image_url"]}" alt="{token["name"]}" class="custom-image">, unsafe_allow_html=True)
-        
+            col.markdown(f'<div class="custom-container"><img src="{token["image_url"]}" alt="{token["name"]}" class="custom-image"><p>{token["name"]} - {token["token_cost"]} points</p></div>', unsafe_allow_html=True)
+            
+            # Move the st.button block inside the container loop
             if st.button(f'Purchase {token["name"]} - {token["token_cost"]} points', key=f'purchase_button_{token["name"]}'):
                 handle_purchase(token["token_id"], token["token_cost"])
-            col.markdown('</div>', unsafe_allow_html=True)
 
 
 
