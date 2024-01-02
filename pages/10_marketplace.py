eb64d3f0245521432d1f642b63b7841883fb240f
mergray0521 <mergray0521@gmail.com>
2024-01-02 13:45:21 -0500

    300

--- pages/10_marketplace.py
+++ pages/10_marketplace.py
@@ -15,7 +15,7 @@ css_code = """
             border-radius: 5px;
             text-align: center;
             margin-bottom: 10px;
-            height: 200px;
+            height: 300px;
         }
 
         .custom-image {
