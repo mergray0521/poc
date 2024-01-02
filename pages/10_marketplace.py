e9daba615eb5e7746ce23a9497aa636e77e298c9
mergray0521 <mergray0521@gmail.com>
2024-01-02 13:50:00 -0500

    utility

--- pages/10_marketplace.py
+++ pages/10_marketplace.py
@@ -24,6 +24,15 @@ css_code = """
             height: 150px;
         }
 
+        .utility-text {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            font-style: italic;
+        }
+
+    </style>
+
     </style>
 """
 
@@ -67,7 +76,7 @@ with st.container():
     for col, token in zip([c1, c2, c3, c4, c5, c6], tokens):
         with col:
             col.markdown(css_code, unsafe_allow_html=True)
-            col.markdown(f'<div class="custom-container"><p>Utility: {token["utility"]}</p><img src="{token["image_url"]}" alt="{token["name"]}" class="custom-image"><p>{token["name"]} - {token["token_cost"]} points</p></div>', unsafe_allow_html=True)
+            col.markdown(f'<div class="custom-container"><p class="utility-text">{token["utility"]}</p><img src="{token["image_url"]}" alt="{token["name"]}" class="custom-image"><p>{token["name"]} - {token["token_cost"]} points</p></div>', unsafe_allow_html=True)
             
             # Move the st.button block inside the container loop
             if st.button(f'Purchase {token["name"]} - {token["token_cost"]} points', key=f'purchase_button_{token["name"]}'):
