533e21fd35a64bedb037e541fec96a856b89fbd3
mergray0521 <mergray0521@gmail.com>
2024-01-03 19:26:17 -0500

    catch up

---
+++ pages/2.5_mint_token_blockchain.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import base64
+from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
+
+# Connect to Kaleido
+USER = "U0a89q50kn"
+PASS = "mSpzLoL2--2IkF4Tne1zgg9geLFl9TYsQR_8lDNZ6mQ"
+RPC_ENDPOINT = "https://u0ujx62qc8-u0elgom1m1-rpc.us0-aws.kaleido.io/"
+
+# Encode the username and password from the app creds into USER:PASS base64 encoded string
+auth = USER + ":" + PASS
+encodedAuth = base64.b64encode(auth.encode('ascii')).decode('ascii')
+
+# Build the header object with the Basic auth and the standard headers
+headers = {'headers': {'Authorization': 'Basic %s' % encodedAuth,
+                       'Content-Type': 'application/json',
+                       'User-Agent': 'kaleido-web3py'}}
+
+# Construct a Web3 object by constructing and passing the HTTP Provider
+provider = HTTPProvider(endpoint_uri=RPC_ENDPOINT, request_kwargs=headers)
+w3 = Web3(provider)
+
+# Add the Geth POA middleware needed for ExtraData Header size discrepancies between consensus algorithms
+w3.middleware_stack.inject(geth_poa_middleware, layer=0)
+
+st.title("Mint Token")
+with st.form("mint_token"):
+    st.header("Create New Token")
+    token_id = st.number_input('Token ID', min_value=606, max_value=1000, value=606, step=1)
+    token_type = st.text_input('Token Type', "")
+    materials = st.text_input('Materials', "")
+    color = st.selectbox('Color', ["Green", "Black", "Silver", "Red", "Brown"]) 
+    mint = st.form_submit_button(label="Mint")
+    if not mint and not st.session_state.get("Mint"):
+        st.stop()
+
+    # Save form data to Kaleido blockchain
+    contract_address = "0x3d7fa7fd572cb6a95ad3e0a971e6ab48094ae67f"  
+    contract_abi = [...]  # Replace with your actual contract ABI
+
+    contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+    transaction = contract.functions.mintToken(token_id, token_type, materials, color).transact()
+
+    st.session_state["Mint"] = True
+    st.success(f"New Token Created and saved to Kaleido: {token_id}")
